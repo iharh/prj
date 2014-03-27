2489c350b9b08b7f1e56a73658d4789b6f80028b
Ihar Hancharenka <iigoncharenko@mail.ru>
2014-03-27 18:34:13 +0300

    autocommit 2014-03-27 18:34:12

--- cabal/husk/b.bat
+++ cabal/husk/b.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal
 call cabal.bat sandbox init
+::call cabal.bat sandbox add-source .deps/your-dep-lib-src
 call cabal.bat install --enable-tests --enable-benchmarks --flags=documentation --haddock-hyperlink-source --only-dependencies
 call cabal.bat build
 call cabal.bat haddock --hyperlink-source
