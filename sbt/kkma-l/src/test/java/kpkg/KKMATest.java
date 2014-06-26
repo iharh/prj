b6d828c95027e2c3497a629ec024d600241ec31d
Ihar Hancharenka <iigoncharenko@mail.ru>
2014-06-26 17:48:45 +0300

    autocommit 2014-06-26 17:48:42

--- sbt/kkma-l/src/test/java/kpkg/KKMATest.java
+++ sbt/kkma-l/src/test/java/kpkg/KKMATest.java
@@ -34,10 +34,10 @@ import static org.junit.Assert.assertNotNull;
 public class KKMATest {
     private static final Logger log = LoggerFactory.getLogger(KKMATest.class);
 
-    private static final String [] TEXTS = { "324-35번지" };
-    //{ "($)", "🍇D", "🍇", "를 만났.", "01:59" };
+    private static final String [] TEXTS = { "7천" };
+    //{ "($)", "🍇D", "🍇", "를 만났.", "01:59", "324-35번지" };
 
-    @Ignore
+    @Test
     public void testDumpSent() throws Exception {
         for (int i = 0; i < TEXTS.length; ++i) {
             String t = TEXTS[i];
@@ -70,7 +70,7 @@ public class KKMATest {
     }
 
 
-    @Test
+    @Ignore
     public void testDumpTokens() throws Exception {
         for (int i = 0; i < TEXTS.length; ++i) {
             String t = TEXTS[i];
