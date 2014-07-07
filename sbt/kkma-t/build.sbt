bf518314eb749d99822e75da04b32cf5156bb378
Ihar Hancharenka <iigoncharenko@mail.ru>
2014-07-07 10:25:16 +0300

    autocommit 2014-07-07 10:25:07

---
+++ sbt/kkma-t/build.sbt
@@ -0,0 +1,32 @@
+//name := "kkma-t"
+
+version := "0.1-SNAPSHOT"
+
+// scalaVersion := "2.10.3"
+
+autoScalaLibrary := false
+
+crossPaths := false
+
+scalacOptions ++= Seq("-encoding", "UTF-8", "-Xlint","-deprecation", "-unchecked")
+
+javacOptions ++= Seq("-encoding", "UTF-8", "-Xlint:unchecked", "-Xlint:deprecation")
+
+testOptions += Tests.Argument(TestFrameworks.JUnit, "-q", "-a")
+
+//logLevel := Level.Debug
+
+// http://www.scala-sbt.org/release/docs/Howto/package.html
+packageOptions in (Compile, packageBin) += Package.ManifestAttributes(java.util.jar.Attributes.Name.CLASS_PATH -> "org.snu.ids.ha3-trial.jar" )
+
+libraryDependencies ++= Seq(
+    "org.slf4j"                 % "slf4j-api"      % "1.7.5",
+    "org.slf4j"                 % "slf4j-log4j12"  % "1.7.5",
+    "log4j"                     % "log4j"          % "1.2.17",
+    // one-jar stuff
+    //"commons-lang"              % "commons-lang"   % "2.6"
+    //
+    "com.novocode"              % "junit-interface"    % "0.10"    % "test",
+    "junit"                     % "junit"              % "4.11"    % "test",
+    "org.hamcrest"              % "hamcrest-library"   % "1.3"     % "test"
+)
---
+++ sbt/kkma-t/src/test/java/kpkg/KKMATest.java
@@ -0,0 +1,90 @@
+package kpkg;
+
+import org.junit.Test;
+import org.junit.Ignore;
+
+
+import org.snu.ids.ha.ma.ModernPostProcessor;
+import org.snu.ids.ha.ma.MExpression;
+import org.snu.ids.ha.ma.MorphemeAnalyzer;
+import org.snu.ids.ha.ma.Sentence;
+import org.snu.ids.ha.ma.Eojeol;
+import org.snu.ids.ha.ma.Morpheme;
+import org.snu.ids.ha.ma.Token;
+import org.snu.ids.ha.ma.Tokenizer;
+
+import org.snu.ids.ha.dic.Dictionary;
+
+import org.snu.ids.ha.constants.POSTag;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+//import static org.junit.Assert.assertThat;
+
+//import static org.hamcrest.core.IsCollectionContaining.hasItem;
+//import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
+
+public class KKMATest {
+    private static final Logger log = LoggerFactory.getLogger(KKMATest.class);
+
+    private static final String [] TEXTS = { "324-35번지" };
+    //{ "($)", "🍇D", "🍇", "를 만났.", "01:59", "324-35번지", "7천" };
+
+    @Test
+    public void testDumpSent() throws Exception {
+        for (int i = 0; i < TEXTS.length; ++i) {
+            String t = TEXTS[i];
+
+            MorphemeAnalyzer ma = new MorphemeAnalyzer();
+
+            // analyze morpheme without any post processing 
+            List<MExpression> mexp = ma.analyze(t);
+
+            log.info("after analyze:\n{}", mexp.toString());
+
+            // refine spacing
+//            mexp = ma.postProcess(mexp);
+            ModernPostProcessor.postProcess(mexp);
+
+            log.info("after post process:\n{}", mexp.toString());
+
+            // leave the best analyzed result
+            mexp = ma.leaveJustBest(mexp);
+
+            // divide result to setences
+            List<Sentence> sentences = ma.divideToSentences(mexp);
+
+            // print the result
+            for (Sentence sentence : sentences) {
+                String sentStr = sentence.getSentence();
+                log.info("kkma sentence: {}", sentStr);
+                log.info("");
+                log.info("!!! eojeols !!!");
+                log.info("");
+                for (Eojeol eojeol : sentence) {
+                    log.debug("eojeol: {}", eojeol.toString());
+                }
+            }
+        }
+    }
+
+
+    @Ignore
+    public void testDumpTokens() throws Exception {
+        for (int i = 0; i < TEXTS.length; ++i) {
+            String t = TEXTS[i];
+
+            DumpUtils.dumpTokens(i, Tokenizer.tokenize(t));
+
+            // DumpUtils.dumpStringCodePoints(t);
+            // DumpUtils.dumpUnicodeBlocks(t);
+        }
+    }
+};
