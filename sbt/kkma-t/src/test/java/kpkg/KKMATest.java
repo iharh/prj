36fab46d5cb39e650f51de22f343db853d41ea37
Ihar Hancharenka <iigoncharenko@mail.ru>
2014-07-07 10:39:15 +0300

    autocommit 2014-07-07 10:39:09

--- sbt/kkma-t/src/test/java/kpkg/KKMATest.java
+++ sbt/kkma-t/src/test/java/kpkg/KKMATest.java
@@ -3,19 +3,21 @@ package kpkg;
 import org.junit.Test;
 import org.junit.Ignore;
 
+import org.snu.ids.ha.core.MAnalyzerFull;
+import org.snu.ids.ha.core.MCandidate;
 
-import org.snu.ids.ha.ma.ModernPostProcessor;
-import org.snu.ids.ha.ma.MExpression;
-import org.snu.ids.ha.ma.MorphemeAnalyzer;
-import org.snu.ids.ha.ma.Sentence;
-import org.snu.ids.ha.ma.Eojeol;
-import org.snu.ids.ha.ma.Morpheme;
-import org.snu.ids.ha.ma.Token;
-import org.snu.ids.ha.ma.Tokenizer;
+//import org.snu.ids.ha.ma.ModernPostProcessor;
+//import org.snu.ids.ha.ma.MExpression;
+//import org.snu.ids.ha.ma.MorphemeAnalyzer;
+//import org.snu.ids.ha.ma.Sentence;
+//import org.snu.ids.ha.ma.Eojeol;
+//import org.snu.ids.ha.ma.Morpheme;
+//import org.snu.ids.ha.ma.Token;
+//import org.snu.ids.ha.ma.Tokenizer;
 
-import org.snu.ids.ha.dic.Dictionary;
+//import org.snu.ids.ha.dic.Dictionary;
 
-import org.snu.ids.ha.constants.POSTag;
+//import org.snu.ids.ha.constants.POSTag;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,23 +36,29 @@ import static org.junit.Assert.assertNotNull;
 public class KKMATest {
     private static final Logger log = LoggerFactory.getLogger(KKMATest.class);
 
-    private static final String [] TEXTS = { "324-35번지" };
+    private static final String [] TEXTS = { "324-35번지", "7천" };
     //{ "($)", "🍇D", "🍇", "를 만났.", "01:59", "324-35번지", "7천" };
 
     @Test
     public void testDumpSent() throws Exception {
+        MAnalyzerFull ma = new MAnalyzerFull();
+
         for (int i = 0; i < TEXTS.length; ++i) {
             String t = TEXTS[i];
 
-            MorphemeAnalyzer ma = new MorphemeAnalyzer();
+            log.info("analizng text:\n{}", t);
 
+            List<MCandidate> rs = ma.analyze(t);
+            for (int k = 0; k < rs.size(); ++k) {
+                log.info("el: {}", rs.get(k).getMorpStr());
+            }
+/*
             // analyze morpheme without any post processing 
             List<MExpression> mexp = ma.analyze(t);
 
             log.info("after analyze:\n{}", mexp.toString());
 
             // refine spacing
-//            mexp = ma.postProcess(mexp);
             ModernPostProcessor.postProcess(mexp);
 
             log.info("after post process:\n{}", mexp.toString());
@@ -72,19 +80,7 @@ public class KKMATest {
                     log.debug("eojeol: {}", eojeol.toString());
                 }
             }
-        }
-    }
-
-
-    @Ignore
-    public void testDumpTokens() throws Exception {
-        for (int i = 0; i < TEXTS.length; ++i) {
-            String t = TEXTS[i];
-
-            DumpUtils.dumpTokens(i, Tokenizer.tokenize(t));
-
-            // DumpUtils.dumpStringCodePoints(t);
-            // DumpUtils.dumpUnicodeBlocks(t);
+*/
         }
     }
 };
