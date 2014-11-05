3a9758031d10693a5c0fd08f45c3fb9e4fcf364b
Ihar Hancharenka <iigoncharenko@mail.ru>
2014-11-05 18:14:00 +0300

    autocommit 2014-11-05 18:13:40

--- sbt/es-migrate/es/migrate/switch.bat
+++ sbt/es-migrate/es/migrate/switch.bat
@@ -1,4 +1,4 @@
 @echo off
 setlocal
-call curl.bat -XPOST "http://admin:admin@epbygomw0024:18080/mobile/rest/index/switch/1404?generation=1"
+call curl.bat -XPOST "http://admin:admin@epbygomw0024:18080/mobile/rest/index/switch/1404?generation=10"
 endlocal
