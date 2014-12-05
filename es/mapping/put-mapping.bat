c2d045ba9d4b8ce544a8099c0a41a9cb7d4cf9f8
Ihar Hancharenka <iigoncharenko@mail.ru>
2014-12-05 11:48:53 +0300

    autocommit 2014-12-05 11:48:50

--- es/mapping/put-mapping.bat
+++ es/mapping/put-mapping.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal
 call vars.bat
-call curl.bat -XPUT "http://%ES_HOSTPORT%/%PRJ_INDEX%" -d @mapping.json
+call curl.bat -XPUT "http://%ES_HOSTPORT%/%PRJ_ID%" -d @map-%PRJ_ID%.json
 endlocal
--- es/mapping/vars.bat
+++ es/mapping/vars.bat
@@ -1,8 +1,9 @@
 @echo off
 set ES_HOST=epbygomw0024
 set ES_PORT=9200
-set PRJ_ID=1234
-set GEN=0
+set PRJ_ID=1900859
+::1234
+::set GEN=0
 
-set PRJ_INDEX=%PRJ_ID%_%GEN%
+;;set PRJ_INDEX=%PRJ_ID%_%GEN%
 set ES_HOSTPORT=%ES_HOST%:%ES_PORT%
