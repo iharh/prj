2ff96fb31511186c95a79a8a083ef699d9b9bff2
Ihar Hancharenka <iigoncharenko@mail.ru>
2014-12-09 18:21:38 +0300

    autocommit 2014-12-09 18:21:28

--- sbt/esmigrate/src/main/java/com/clarabridge/elasticsearch/index/migrate/plugin/MigrateRestHandler.java
+++ sbt/esmigrate/src/main/java/com/clarabridge/elasticsearch/index/migrate/plugin/MigrateRestHandler.java
@@ -8,19 +8,25 @@ import org.elasticsearch.rest.RestRequest;
 import org.elasticsearch.rest.RestChannel;
 import org.elasticsearch.rest.BytesRestResponse;
 
+import org.elasticsearch.indices.IndicesService;
+
 import static org.elasticsearch.rest.RestRequest.Method.GET;
 import static org.elasticsearch.rest.RestStatus.OK;
 
 public class MigrateRestHandler implements RestHandler {
+    private IndicesService indicesService;
+
     @Inject
-    public MigrateRestHandler(RestController restController) {
+    public MigrateRestHandler(RestController restController, IndicesService indicesService) {
         restController.registerHandler(GET, "/_hello", this); // $NON-NLS-1$
+        this.indicesService = indicesService;
     }
 
     @Override
     public void handleRequest(final RestRequest request, final RestChannel channel) {
         String who = request.param("who"); // $NON-NLS-1$
-        String whoSafe = (who != null) ? who : "world"; // $NON-NLS-1$
+        //String whoSafe = (who != null) ? who : "world"; // $NON-NLS-1$
+        String whoSafe = (indicesService != null) ? "indicesService" : "null"; // $NON-NLS-1$ $NON-NLS-2$
 
         channel.sendResponse(new BytesRestResponse(OK, "Hello, " + whoSafe)); // $NON-NLS-1$
     }
