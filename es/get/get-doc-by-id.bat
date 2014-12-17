a85f430bc48c89e385b29e3351bf093b14ca700d
Ihar Hancharenka <iigoncharenko@mail.ru>
2014-12-17 18:03:51 +0300

    autocommit 2014-12-17 18:03:49

--- es/get/get-doc-by-id.bat
+++ es/get/get-doc-by-id.bat
@@ -1,5 +1,6 @@
 @echo off
 setlocal
 call vars.bat
+call curl.bat -XGET "http://%ES_HOSTPORT%/%PRJ_ID%/document/%DOC_ID%?pretty"
 call curl.bat -XGET "http://%ES_HOSTPORT%/%PRJ_INDEX%/document/%DOC_ID%?pretty"
 endlocal
