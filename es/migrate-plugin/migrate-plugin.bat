ae1b0e69694fe26aeee7535e6bdc63931312b691
Ihar Hancharenka <iigoncharenko@mail.ru>
2015-01-09 17:44:23 +0300

    autocommit 2015-01-09 17:44:16

--- es/migrate-plugin/migrate-plugin.bat
+++ es/migrate-plugin/migrate-plugin.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal
 call vars.bat
-call curl.bat -XPOST "http://%ES_HOSTPORT%/_migrate?projectId=2&sleepBetweenBatches=1s"
+call curl.bat -XPOST "http://%ES_HOSTPORT%/_migrate?projectId=1"
 ::&sleepBetweenBatches=1s
 endlocal
+
