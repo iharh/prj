00dfc2428f61c93484706fa4116b8560b0454b7e
Ihar Hancharenka <iigoncharenko@mail.ru>
2015-01-15 18:31:30 +0300

    autocommit 2015-01-15 18:31:25

--- es/query/q_id_document.bat
+++ es/query/q_id_document.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal
 call vars.bat
-call curl.bat -XPOST "http://%ES_HOSTPORT%/%PRJ_ID%/document/_search?pretty" -d @q_id_document.json
+call curl.bat -XPOST "http://%ES_HOSTPORT%/%PRJ_ID%/document,verbatim/_search?pretty" -d @q_id_document.json
 endlocal
