9e8cd62c74ce62025a3643c0c83f0cfa421b154b
Ihar Hancharenka <iigoncharenko@mail.ru>
2015-09-22 12:19:56 +0300

    autocommit 2015-09-22 12:19:55

--- docker/ublas/a.cpp
+++ docker/ublas/a.cpp
@@ -6,24 +6,60 @@
 using namespace boost::numeric::ublas;
 
 typedef matrix<float> t_matrix;
+typedef vector<float> t_vector;
 
 void
-print_matrix_2_2(const std::wstring &sText, t_matrix a)
+printM(const std::wstring &sText, t_matrix m)
 {
-    std::wcout << sText << std::endl;
-    std::wcout << a(0, 0) << L" " << a(0, 1) << std::endl;
-    std::wcout << a(1, 0) << L" " << a(1, 1) << std::endl;
+    size_t s1 = m.size1();
+    size_t s2 = m.size2();
+
+    std::wcout << sText << L" [" << s1 << L", " << s2 << L"]" << std::endl;
+    for (size_t i = 0; i < s1; ++i)
+    {
+        for (size_t j = 0; j < s2; ++j)
+        {
+            std::wcout << L" " << m(i, j);
+        }
+        std::wcout << std::endl;
+    }
     std::wcout << std::endl;
 }
+
+void
+printV(const std::wstring &sText, t_vector v)
+{
+    size_t s = v.size();
+
+    std::wcout << sText << L" [" << s << L"]" << std::endl;
+    for (size_t i = 0; i < s; ++i)
+    {
+        std::wcout << L" " << v(i);
+    }
+    std::wcout << std::endl;
+}
+
 int
 main()
 {
-    t_matrix a(2, 2);
+    t_matrix a(3, 2);
     a(0, 0) = 1.;
     a(0, 1) = 2.;
     a(1, 0) = 3.;
     a(1, 1) = 4.;
-    print_matrix_2_2(L"a:", a);    
+    a(2, 0) = 5.;
+    a(2, 1) = 6.;
+    printM(L"a:", a);
+
+    t_vector v(2);
+    for (size_t i = 0; i < v.size(); ++i)
+    {
+        v(i) = i + 1.;
+    }
+    printV(L"v:", v);
+
+    t_vector z = prod(a, v);
+    printV(L"z:", z);
 
     return 0;
 }
