c5839ff2b4e0e39f5d1c87f74ab1d0e8d7e3e29d
Ihar Hancharenka <iigoncharenko@mail.ru>
2015-09-30 17:58:06 +0300

    autocommit 2015-09-30 17:58:03

--- gradle/qp/src/test/groovy/antlr4/SwimlineQPTests.groovy
+++ gradle/qp/src/test/groovy/antlr4/SwimlineQPTests.groovy
@@ -18,27 +18,27 @@ class SwimlineQPTests extends Specification {
             QP.parse(input, s, w, q, m, e)
         then:
             s == simple as Set
-            w == [] as Set
+            w == wildcard as Set
             q == quoted as Set
-            m == mt as Set
+            m == mtoken as Set
             e.size() == ecnt
         where:
-            input                                | simple                | quoted                | mt | ecnt
-            "Wo*rd1, wprds2?, \"wasdh^&^or*d3\"" | ["Wo*rd1", "wprds2?"] | ["\"wasdh^&^or*d3\""] | [] | 0 // basic
-            "\"<:)>\""                           | []                    | ["\"<:)>\""]          | [] | 0 // smiley and quoted
-            "&& || OR AND and or | |"            | []                    | []                    | [] | 1 // tricky
-            "ATTR:\"abc\\\"def\""                | []                    | []                    | [] | 0 // field last quote
-            "ATTR:\"+-!(){}[]^\\\"~*?\\ \""      | []                    | []                    | [] | 0 // field no escape inside quotes
-            "ATTR:\\+\\-\\!\\(\\)\\{\\}\\[\\]\\^\\\"\\~\\*\\?\\:\\\\"                                                          | [] | []                   | [] | 0 // field escaped
-            "REF_URL:http\\://mail.aol*, REF_URL:http\\://mail.aol?, REF_URL:\"http://mail.aol\""                              | [] | []                   | [] | 0 // field url
-            "business_date: [32 to kjdfhsdjf] doc_date: [32 23] (Cuban AND Crisis ) \" AND ()asjd \"  Location: \"New York\" " | [] | ["\" AND ()asjd \""] | [] | 1 // range &&
-            "_catRef:[model:\"v1Products Model - Tuning Base Model\" path:\"v1Enterprise|Poweredge Modular Servers\" node:\"v1Mellanox IB m2401g\"], w1" | ["w1"] | [] | [] | 0 // catref
-            "_lc:[room, clean]"     | [] | [] | [] | 0 // lc 1
-            "_lc:[abc\\ -->\\ def]" | [] | [] | [] | 1 // lc 2
-            "_lc:(acheter\\ -->\\ fleur_?) _lc:(hello\\ -->\\ word_1) _lc:(good*\\ -->\\ xxx_?)"  | []                             | [] | [] | 3 // lc 2
-            "_catRollup:\"abc - def\", _catRollup :abc1, w2"                                      | ["w2"]                         | [] | [] | 0 // catrollup
-            "w1, w2, w3 and w4 and (w5, w6 and (w7 and w8)) and w9 or (w10, w11)"                 | ["w1", "w2", "w3", "w4", "w9"] | [] | [] | 0 // levels2
-            "w1 or w2 or w3 and w4 and (w5 or w6 and (w7 and w8)) and w9 or (w10 and w11)"        | ["w1", "w2", "w3", "w4", "w9"] | [] | [] | 0 // levels1
-            "_mtoken:\":)\"" | [] | [] | ["\":)\""] | 0
+            input                                | simple | wildcard              | quoted                | mtoken     | ecnt
+            "_mtoken:\":)\""                     | []     | []                    | []                    | ["\":)\""] | 0
+            "Wo*rd1, wprds2?, \"wasdh^&^or*d3\"" | []     | ["Wo*rd1", "wprds2?"] | ["\"wasdh^&^or*d3\""] | []         | 0 // basic
+            "\"<:)>\""                           | []     | []                    | ["\"<:)>\""]          | []         | 0 // smiley and quoted
+            "&& || OR AND and or | |"            | []     | []                    | []                    | []         | 1 // tricky
+            "ATTR:\"abc\\\"def\""                | []     | []                    | []                    | []         | 0 // field last quote
+            "ATTR:\"+-!(){}[]^\\\"~*?\\ \""      | []     | []                    | []                    | []         | 0 // field no escape inside quotes
+            "ATTR:\\+\\-\\!\\(\\)\\{\\}\\[\\]\\^\\\"\\~\\*\\?\\:\\\\"                                                          | [] | [] | []                   | [] | 0 // field escaped
+            "REF_URL:http\\://mail.aol*, REF_URL:http\\://mail.aol?, REF_URL:\"http://mail.aol\""                              | [] | [] | []                   | [] | 0 // field url
+            "business_date: [32 to kjdfhsdjf] doc_date: [32 23] (Cuban AND Crisis ) \" AND ()asjd \"  Location: \"New York\" " | [] | [] | ["\" AND ()asjd \""] | [] | 1 // range &&
+            "_catRef:[model:\"v1Products Model - Tuning Base Model\" path:\"v1Enterprise|Poweredge Modular Servers\" node:\"v1Mellanox IB m2401g\"], w1" | ["w1"] | [] | [] | [] | 0 // catref
+            "_lc:[room, clean]"     | [] | [] | [] | [] | 0 // lc 1
+            "_lc:[abc\\ -->\\ def]" | [] | [] | [] | [] | 1 // lc 2
+            "_lc:(acheter\\ -->\\ fleur_?) _lc:(hello\\ -->\\ word_1) _lc:(good*\\ -->\\ xxx_?)"  | []                             | [] | [] | [] | 3 // lc 2
+            "_catRollup:\"abc - def\", _catRollup :abc1, w2"                                      | ["w2"]                         | [] | [] | [] | 0 // catrollup
+            "w1, w2, w3 and w4 and (w5, w6 and (w7 and w8)) and w9 or (w10, w11)"                 | ["w1", "w2", "w3", "w4", "w9"] | [] | [] | [] | 0 // levels2
+            "w1 or w2 or w3 and w4 and (w5 or w6 and (w7 and w8)) and w9 or (w10 and w11)"        | ["w1", "w2", "w3", "w4", "w9"] | [] | [] | [] | 0 // levels1
     }
 }
