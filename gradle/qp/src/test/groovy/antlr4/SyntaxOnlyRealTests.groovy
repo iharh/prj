aa33905d26c4cc85dbc09a03ab9bf3e0f7fe1c35
Ihar Hancharenka <iigoncharenko@mail.ru>
2015-10-02 18:34:18 +0300

    autocommit 2015-10-02 18:34:16

--- gradle/qp/src/test/groovy/antlr4/SyntaxOnlyRealTests.groovy
+++ gradle/qp/src/test/groovy/antlr4/SyntaxOnlyRealTests.groovy
@@ -2,6 +2,7 @@ package antlr4
 
 import spock.lang.Specification
 import spock.lang.Unroll
+import spock.lang.Shared
 
 import static rx.observables.StringObservable.from
 import static rx.observables.StringObservable.byLine
@@ -13,15 +14,21 @@ class SyntaxOnlyRealTests extends Specification {
     def m = [] as Set
     def e = []
 
+    @Shared def realSwimlines = []
+	
+    def setupSpec() {
+	//realSwimlines = new File(new ClassPathResource('swimlines/swimlines.txt').getURI()) as String[]
+	realSwimlines = Utils.getLinesCP("q-qa10.txt")
+    }
+
     @Unroll
-    def "qa10 idx: n rule: #l"() {
+    def "qa10 idx: #n rule: #l"() {
         when:
             QP.parse(l, s, w, q, m, e)
         then:
             e.size() == 0
         where:
-            l << Utils.getLinesCP("q-qa10.txt")
-            //l << Utils.getLinesCP("q-epbygomw0024.txt")
-            //n << (1 .. 5000)
+            l << realSwimlines
+            n << (1 .. realSwimlines.size())
     }
 }
