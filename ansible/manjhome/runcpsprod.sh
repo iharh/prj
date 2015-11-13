d87b8a713b57b64dd3abab2cbdd489f083b81f5f
Ihar Hancharenka <iigoncharenko@mail.ru>
2015-11-13 17:38:30 +0300

    add multi-layered playbooks

--- ansible/manjhome/runcpsprod.sh
+++ ansible/manjhome/runcpsprod.sh
@@ -1,4 +1,2 @@
 #! /bin/sh
-ansible-playbook -vvvv -i inventories/cpsprod pb-dbg.yml -e "host=all user=ihar.hancharenka"
-#user=prediction
-# user=ihar.hancharenka also works
+ansible-playbook -vvvv -i inventories/cpsprod pb-site-full.yml -e "hosts=all user=ihar.hancharenka"
