59c20b92d157ba8826777af011b84e8c32ba4ef4
Ihar Hancharenka <iigoncharenko@gmail.com>
2015-11-13 13:33:37 +0300

    add cfg

--- ansible/manjhome/runcpsprod.sh
+++ ansible/manjhome/runcpsprod.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
-ansible-playbook -vvvv -i inventories/cpsprod pb-dbg.yml -e "host=all user=prediction"
+ansible-playbook -vvvv -i inventories/cpsprod pb-dbg.yml -e "host=all user=ihar.hancharenka"
+#user=prediction
 # user=ihar.hancharenka also works
