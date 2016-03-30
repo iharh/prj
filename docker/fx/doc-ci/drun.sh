501dcc225b4e76612b63bb8197febf059c90b772
Ihar Hancharenka <iigoncharenko@gmail.com>
2016-03-30 11:04:18 +0300

    insideness check

--- docker/fx/doc-ci/drun.sh
+++ docker/fx/doc-ci/drun.sh
@@ -4,19 +4,17 @@ set -e
 CTX_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 . $CTX_DIR/env-vars.sh
 
+#TODO: move to env-vars
+SVN_BASE=/data/wrk/svnmain
+
 RUN_CMD="$@"
 if [[ -z "$RUN_CMD" ]]; then
   RUN_FLAGS="--rm -ti"
   RUN_PREF=
   RUN_CMD=/bin/bash
 else
-  #RUN_FLAGS=
   RUN_PREF="/bin/bash -cl"
 fi
-#echo RUN: $RUN_PREF "$RUN_CMD"
-#echo RUN_FLAGS: $RUN_FLAGS
-#echo RUN_PREF: $RUN_PREF
-#echo RUN_CMD: $RUN_CMD
 
 #WRK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 #CMN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../.." && pwd )"
@@ -24,11 +22,16 @@ fi
 
 #USER_HOME=/home/$GUEST_USER_NAME
 
+if [[ "${PWD:0:${#SVN_BASE}}" != "$SVN_BASE" ]]; then
+    echo Able to work only inside: $SVN_BASE
+    exit 1
+fi
+
 #-v $CACHE_DIR/.gradle:$USER_HOME/.gradle\
 docker run $RUN_FLAGS\
  -v $(pwd):/prj\
  -w /prj\
  $CUR_GROUP/$CUR_NAME_CI:$CUR_VER\
  $RUN_PREF "$RUN_CMD"
+
 # TODO: use $(realpath --relative-to=/data/wrk/svnmain $(pwd)
-# http://stackoverflow.com/questions/2172352/in-bash-how-can-i-check-if-a-string-begins-with-some-value
