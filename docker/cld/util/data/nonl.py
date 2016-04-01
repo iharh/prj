0071e0b88a8de6ac195404c1b482050a661979bc
Ihar Hancharenka <iigoncharenko@gmail.com>
2016-04-02 01:08:03 +0300

    params

--- docker/cld/util/data/nonl.py
+++ docker/cld/util/data/nonl.py
@@ -1,9 +1,15 @@
+import sys
 import csv
-ifilename = 'other.csv'
-with open(ifilename, newline='', encoding='utf-8') as ifile:
-    rdr = csv.reader(ifile, delimiter=',', quotechar='"')
-    try:
-        for row in rdr:
-            print(', '.join(row).replace('\n', ' ').replace('\r', ' '))
-    except csv.Error as e:
-        sys.exit('error at file {}, line {}: {}'.format(ifilename, rdr.line_num, e))
+
+ifilename = sys.argv[1]
+ofilename = sys.argv[2]
+
+with open(ofilename, 'w', newline='') as ofile:
+    wrtr = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    with open(ifilename, newline='', encoding='utf-8') as ifile:
+        rdr = csv.reader(ifile, delimiter=',', quotechar='"')
+        try:
+            for row in rdr:
+                wrtr.writerow(map(lambda x : x.replace('\n', ' '), row))
+        except csv.Error as e:
+            sys.exit('error at file {}, line {}: {}'.format(ifilename, rdr.line_num, e))
