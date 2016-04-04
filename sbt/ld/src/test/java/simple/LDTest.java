39fecb3d2c1c6162dab738e6f002927b7dc7b317
Ihar Hancharenka <iigoncharenko@gmail.com>
2016-04-04 21:07:00 +0300

    split into functions

--- sbt/ld/src/test/java/simple/LDTest.java
+++ sbt/ld/src/test/java/simple/LDTest.java
@@ -9,6 +9,8 @@ import com.clarabridge.transformer.ld.StringSourceIterator;
 import com.clarabridge.transformer.ld.NormCrossModelScorer.Result;
 import com.clarabridge.transformer.ld.compiler.Compiler;
 import com.clarabridge.transformer.ld.utils.FileNamesCollector;
+import com.clarabridge.transformer.ld.exceptions.ModelCreatorException;
+import com.clarabridge.transformer.ld.exceptions.MathException;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
@@ -17,6 +19,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.File;
 //import java.io.Reader;
 //import java.io.FileReader;
+import java.io.IOException;
 
 import static org.junit.Assert.assertTrue;
 import static java.nio.charset.StandardCharsets.*;
@@ -27,37 +30,43 @@ import org.slf4j.LoggerFactory;
 public class LDTest {
     private static final Logger log = LoggerFactory.getLogger(LDTest.class);
 
-    @Test
-    public void testDetect() throws Exception {
-        File modelDir = new File("/data/wrk/clb/ld");
+    private static NormLangDetector getLangDetector(String modelDirName) throws IOException, ModelCreatorException {
         FileNamesCollector modelCollector = new FileNamesCollector();
 
-        modelCollector.addFileMask(modelDir, "*.ldm");
+        File modelDir = new File(modelDirName);
         log.info("Loading language detector models from \"{}\"", modelDir.getCanonicalPath());
+
+        modelCollector.addFileMask(modelDir, "*.ldm");
         log.info("Found {} model(s)", modelCollector.getFiles().size());
 
         CrossModel crossModel = Compiler.createModel(modelCollector.getFiles());
         final double desiredConfidenceLevel = 0.01; // 0.5;
-        NormLangDetector langDetector = new NormLangDetector(crossModel, desiredConfidenceLevel);
+        return new NormLangDetector(crossModel, desiredConfidenceLevel);
+    }
 
+    private static void process(final NormLangDetector langDetector, String inFileName) throws IOException, ModelCreatorException, MathException {
         final CSVFormat csvFormat = CSVFormat.DEFAULT
             .withIgnoreSurroundingSpaces(true)
             .withHeader("TEXT");
 
-        final String iFileName = "/data/wrk/prj/docker/cld/util/data/en.csv";
-        //final Reader inReader = new FileReader(iFileName);
-        final CSVParser csvParser = CSVParser.parse(new File(iFileName), UTF_8, csvFormat);
+        final CSVParser csvParser = CSVParser.parse(new File(inFileName), UTF_8, csvFormat);
         for (final CSVRecord r : csvParser) {
-            //log.info("r TEXT: {}", r.get("TEXT"));
-
             final String text = r.get(0);
+
             final int maxWidthToTest = text.length();
             final StringSourceIterator sourceIterator =  new StringSourceIterator(text, maxWidthToTest);
 
-            Result res = langDetector.analyse(sourceIterator);
+            Result res = langDetector.analyse(sourceIterator); // throws ModelCreatorException, MathException
 
             log.info("lang code: {}, conf: {}", res.getLangCode(), Double.toString(res.getConfidenceLevel()));
         }
+    }
+
+    @Test
+    public void testDetect() throws Exception {
+        final NormLangDetector langDetector = getLangDetector("/data/wrk/clb/ld");
+        final String inFileName = "/data/wrk/prj/docker/cld/util/data/en.csv";
+        process(langDetector, inFileName);
         assertTrue(true);
     }
 }
