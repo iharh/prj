8c08e7dc3d622df31a845d2fe00d03db34ebb26d
Ihar Hancharenka <iigoncharenko@gmail.com>
2016-05-12 17:06:43 +0300

    finalize

--- gradle/rest/src/test/groovy/rest/RESTTests.groovy
+++ gradle/rest/src/test/groovy/rest/RESTTests.groovy
@@ -9,45 +9,52 @@ import groovyx.net.http.EncoderRegistry
 
 import groovyx.gpars.GParsExecutorsPool
 
+import org.apache.http.HttpResponse
+
 import spock.lang.Specification
 //import spock.lang.Unroll
 
 class RESTTests extends Specification {
-    //@Unroll
-    //def "crew member name: '#name' length: #length"() {
+    def suggestSimilarWords() {
+        def cl = new RESTClient('https://prediction-engines.dev.clarabridge.net:8443/')
+        cl.defaultRequestHeaders['Authorization'] = 'Bearer ZGV2OjEyMzQ1'
+        def encoder = cl.getEncoder() // = new EncoderRegistry()
+        def entity = encoder.encodeJSON([
+            words: ['phone', 'phones'],
+            suggesterName: 'Default',
+            aggregationMode: 'CENTRALIZED',
+            top: '2',
+            communityTop: '2'
+        ], null)
+        def json = entity.content.text
+        cl.post(
+            path: 'v1/csls/suggestSimilarWords',
+            contentType: ContentType.JSON,
+            body: json)
+    }
+
+    def assertResp(HttpResponse resp) {
+        assert 200 == resp.status
+        resp.data == [
+            responseHash: '1c8f2d18950224cceccb65f61f5342774b9a67c5a62044dcde2dbecd1f6ccf28',
+            suggestedWords: [
+                [name: 'CELL PHONE', similarity:0.779014554325182],
+                [name:'CELLPHONE', similarity:0.7722717367639094]
+            ]
+        ]
+    }
 
     def "test rest"() {
         when:
-            GParsExecutorsPool.withPool(12) {
-                (1..1000).eachParallel {
-                    def cl = new RESTClient('https://prediction-engines.dev.clarabridge.net:8443/')
-                    cl.defaultRequestHeaders['Authorization'] = 'Bearer ZGV2OjEyMzQ1'
-                    def encoder = cl.getEncoder() // = new EncoderRegistry()
-                    def entity = encoder.encodeJSON([
-                        words: ['phone', 'phones'],
-                        suggesterName: 'Default',
-                        aggregationMode: 'CENTRALIZED',
-                        top: '2',
-                        communityTop: '2'
-                    ], null)
-                    def json = entity.content.text
-                    def resp = cl.post(
-                        path: 'v1/csls/suggestSimilarWords',
-                        contentType: ContentType.JSON,
-                        body: json)
+            def resp = suggestSimilarWords()
 
-                    assert 200 == resp.status
+            GParsExecutorsPool.withPool(12) {
+                (1..1000).eachParallel { // parallel.each
+                    def itResp = suggestSimilarWords()
+                    assertResp(itResp)
                 }
             }
         then:
-            1 == 1
-            //resp.status == 200
-            //resp.data == [
-            //    responseHash: '1c8f2d18950224cceccb65f61f5342774b9a67c5a62044dcde2dbecd1f6ccf28',
-            //    suggestedWords: [
-            //        [name: 'CELL PHONE', similarity:0.779014554325182],
-            //        [name:'CELLPHONE', similarity:0.7722717367639094]
-            //    ]
-            //]
+            assertResp(resp)
     }
 }
