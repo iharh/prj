b968790f4617ec36994321d194a7b024f8f9ef51
Ihar Hancharenka <iigoncharenko@gmail.com>
2016-05-13 16:50:10 +0300

    more data

--- gradle/rest/src/test/groovy/rest/RESTTests.groovy
+++ gradle/rest/src/test/groovy/rest/RESTTests.groovy
@@ -15,18 +15,18 @@ import spock.lang.Specification
 //import spock.lang.Unroll
 
 class RESTTests extends Specification {
-    def suggestSimilarWords(String langCode) {
+    def suggestSimilarWords(String langCode, List words) {
         def cl = new RESTClient('https://prediction-engines.dev.clarabridge.net:8443/')
         cl.defaultRequestHeaders['Authorization'] = 'Bearer ZGV2OjEyMzQ1'
         def encoder = cl.getEncoder() // = new EncoderRegistry()
         def entity = encoder.encodeJSON([
-            words: ['phone', 'phones'],
+            lang: langCode,
+            words: words,
             suggesterName: 'Default',
             aggregationMode: 'CENTRALIZED',
             top: '2',
             communityTop: '2'
         ], null)
-        //lang: "${langCode}",
         def json = entity.content.text
         cl.post(
             path: 'v1/csls/suggestSimilarWords',
@@ -34,29 +34,47 @@ class RESTTests extends Specification {
             body: json)
     }
 
-    def enSSW() { suggestSimilarWords('en') }
+    def enSSW() { suggestSimilarWords('en', ['phone', 'phones']) }
+    def deSSW() { suggestSimilarWords('de', ['hoh']) }
+    def frSSW() { suggestSimilarWords('fr', ['lamour']) }
 
-    def assertResp(HttpResponse resp) {
+    def assertResp(HttpResponse resp, String hash, List words) {
         assert 200 == resp.status
         resp.data == [
-            responseHash: '1c8f2d18950224cceccb65f61f5342774b9a67c5a62044dcde2dbecd1f6ccf28',
-            suggestedWords: [
-                [name: 'CELL PHONE', similarity:0.779014554325182],
-                [name:'CELLPHONE', similarity:0.7722717367639094]
-            ]
+            responseHash: hash,
+            suggestedWords: words
         ]
     }
 
-    def "test rest en"() {
+    def enAssert(HttpResponse resp) { assertResp(resp,
+        '1c8f2d18950224cceccb65f61f5342774b9a67c5a62044dcde2dbecd1f6ccf28',
+        [
+            [ name: 'CELL PHONE', similarity: 0.779014554325182 ],
+            [ name: 'CELLPHONE' , similarity: 0.7722717367639094]
+        ])}
+    def deAssert(HttpResponse resp) { assertResp(resp,
+        '4a66aafeaf46a92d37d4d8ec067fe4773262201afaaf42e3f18405e8b8075115',
+        [
+            [ name: 'KLEINWEILAND', similarity: 0.5147832573493378],
+            [ name: 'MMTZ'        , similarity: 0.5131634794648856]
+        ])}
+    def frAssert(HttpResponse resp) { assertResp(resp,
+        '822eb839cd6b0fc667841295616b6090103abf699e241c7444ad87b6c1dab0f1',
+        [
+            [ name: 'DAMOUR' , similarity: 0.7053097337264178],
+            [ name: 'L’AMOUR', similarity: 0.0               ]
+        ])}
+
+    def "test rest"() {
         when:
-            def resp = enSSW()
+            def resp = frSSW()
             /*GParsExecutorsPool.withPool(12) {
                 (1..1000).eachParallel { // parallel.each
-                    def itResp = enSSW()
-                    assertResp(itResp)
+                    def itResp = deSSW()
+                    deAssert(itResp)
                 }
             }*/
         then:
-            assertResp(resp)
+            frAssert(resp)
     }
 }
