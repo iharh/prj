1e2cf2f5264db6f213e062b8431c50ef938c97f6
Ihar Hancharenka <iigoncharenko@gmail.com>
2016-05-25 14:14:20 +0300

    improve train

--- py/vec/train.py
+++ py/vec/train.py
@@ -1,5 +1,3 @@
-__author__ = 'Mingsheng Wu'
-
 # to run word2vec fast version properly, one has to ensure that the machine has proper cmake, cython, and blas installed
 # in addition, one has to ensure the proper version of scipy is installed since some versions of scipy has a deprecated
 # version of blas.
@@ -16,25 +14,26 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 # a method to build a word2vec model from a list of file names, but the vocabulary of the model is only taken from the
 # first file
 # the resulting model is saved to parameter model_name in the same directory as the working directory
-def build_model_en(files, model_name):
-    if len(files) == 1:
-        sentences = LineSentence(files[0])
-        model = Word2Vec(sentences, size=300, window=10, min_count=50, workers=4)
-    else:
-        first=True
-        for file in files:
-            sentences=LineSentence(file)
-            if first:
-                model = Word2Vec(sentences, size=300, window=10, min_count=50, workers=4)
-            else:
-                model.train(sentences)
-    print "Model successfully built."
-    model.save_word2vec_format(model_name,binary=True)
-    return model
-
+#def build_model(files, model_name):
+#    if len(files) == 1:
+#        sentences = LineSentence(files[0])
+#        model = Word2Vec(sentences, size=300, window=10, min_count=50, workers=4)
+#    else:
+#        first=True
+#        for file in files:
+#            sentences=LineSentence(file)
+#            if first:
+#                model = Word2Vec(sentences, size=300, window=10, min_count=50, workers=4)
+#            else:
+#                model.train(sentences)
+#    print "Model successfully built."
+#    model.save_word2vec_format(model_name,binary=True)
+#    return model
 
 def train():
-    out_data = 'out/data/de_data'
+    out_data  = 'out/data/en/en_small_data'
+    out_model = 'out/model/en/en_small_news'
+
     print "loading sentences..."
     sentences = LineSentence(out_data);
     print "phrases..."
@@ -42,7 +41,7 @@ def train():
     print "start training..."
     model = Word2Vec(bigram_transformer[sentences], size=300, window=10, min_count=10, workers=6)
     print "saving the model..."
-    model.save_word2vec_format("out/News_" + out_data + ".bin", binary=True)
+    model.save_word2vec_format(out_model, binary=True)
     print "All complete"
 
 train()
