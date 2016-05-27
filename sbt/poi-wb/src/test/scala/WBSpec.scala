eaab1b59b6d3c2fc3c2dcb8060f4496c822b606b
Ihar Hancharenka <iigoncharenko@mail.ru>
2016-05-27 11:27:50 +0300

    wb

--- sbt/poi-wb/src/test/scala/WBSpec.scala
+++ sbt/poi-wb/src/test/scala/WBSpec.scala
@@ -1,6 +1,8 @@
 import org.scalatest._
 
 //import kantan.csv.ops._
+import org.apache.poi.ss.util._
+import org.apache.poi.xssf.usermodel._
 
 import java.io.File
 
@@ -9,11 +11,17 @@ import org.slf4j.LoggerFactory
 
 //import scala.collection.JavaConversions._
 
-class WBSpec extends FlatSpec {
+class WBSpec extends FlatSpec with Matchers {
     private val log = LoggerFactory.getLogger(classOf[WBSpec])
 
     "WB" should "read a spreadsheet" in {
         log.info("start")
+
+        val inFile = new File("in" + File.separator + "Taxonomies.xlsx")
+        val wb = new XSSFWorkbook(inFile)
+
+        val sheet = wb.getSheetAt(0)
+        sheet should not be (null)
 
         /*val out = new File("out.csv")
         val writer = out.asCsvWriter[(Int, String)](',', List("key", "val"))
@@ -22,7 +30,6 @@ class WBSpec extends FlatSpec {
         writer.close()*/
 
         log.info("finish")
-        assert(true === true)
     }
 }
 
