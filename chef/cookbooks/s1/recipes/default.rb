22144aada3d3eadc521a485ee9a20745fa8c173b
Ihar Hancharenka <iigoncharenko@gmail.com>
2016-06-17 20:22:36 +0300

    chef cookbook s1

---
+++ chef/cookbooks/s1/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: s1
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+file '/home/vagrant/hello-world.txt' do
+    content 'hello world'
+end
