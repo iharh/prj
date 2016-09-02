5fca5e684b161a5d6fbaedb7ce6bfce454d2c76c
Ihar Hancharenka <iigoncharenko@gmail.com>
2016-09-02 09:27:01 +0300

    simplify

--- stack/d1/t.sh
+++ stack/d1/t.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
-. ~/.sh.d/stack.sh
+#. ~/.sh.d/stack.sh
 #setopt aliases
 # https://github.com/jarmo/expand-aliases-oh-my-zsh
 #which stack
+#echo $STACK_ROOT
 
 stack build
 stack exec d1-exe -- -o d1.svg -w 400
