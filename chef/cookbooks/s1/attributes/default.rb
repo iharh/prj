a33aac9b35bd51a6778886966b2f4a336d7f309b
Ihar Hancharenka <iigoncharenko@mail.ru>
2016-09-16 18:39:40 +0300

    autocommit 2016-09-16 18:39:13

---
+++ chef/cookbooks/s1/attributes/default.rb
@@ -0,0 +1,1 @@
+node.default['cb']['s1']['ids'] = %w(01 02 -de-01)
--- chef/cookbooks/s1/recipes/default.rb
+++ chef/cookbooks/s1/recipes/default.rb
@@ -1,13 +1,9 @@
-#
-# Cookbook Name:: s1
-# Recipe:: default
-#
-# Copyright 2016, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
 include_recipe 'yum'
 
-file '/home/vagrant/hello-world.txt' do
-    content 'hello world'
+s1_ids = node['cb']['s1']['ids']
+
+s1_ids.each do |s1_id|
+    file "/home/vagrant/hello#{s1_id}.txt" do
+        content "hello #{s1_id}"
+    end
 end
