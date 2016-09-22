70c094f9bbb80dd6d4b28db64b5a7c89e6aaf9fc
Ihar Hancharenka <iigoncharenko@gmail.com>
2016-09-22 20:05:01 +0300

    s1

--- swift/s1/hello.swift
+++ swift/s1/hello.swift
@@ -1,2 +1,16 @@
 #! /usr/bin/swift
+import Foundation
+
 print("Hello world")
+let args = CommandLine.arguments
+for a in args {
+    print(a)
+}
+let fm = FileManager()
+let curp: String = fm.currentDirectoryPath
+print(curp)
+let enumerator = fm.enumerator(atPath: curp) // -> FileManager.DirectoryEnumerator?
+while let element = enumerator?.nextObject() as? String { 
+//    // where element.pathExtension == "txt"
+    print(element)
+}
