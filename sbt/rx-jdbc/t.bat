0183f260a8a47b06e4133f410cad2db7680561af
Ihar Hancharenka <iigoncharenko@mail.ru>
2016-12-20 17:42:20 +0300

    autocommit 2016-12-20 17:42:17

--- sbt/rx-jdbc/t.bat
+++ sbt/rx-jdbc/t.bat
@@ -4,9 +4,10 @@ del /Q %~dp0*.log
 del /Q %~dp0out\*.xml
 ::call sbt.bat test
 set EXTRA_OPTS="-Djava.library.path=D:/clb/inst/fx"
+:: pg/epbygomw0024
 :: pg/qa-mohamed
-call sbt.bat "-Ddbcfg=pg/epbygomw0024" "testOnly *FileRepoListTest"
-::call sbt.bat "testOnly *MappingTest"
+call sbt.bat "-Ddbcfg=pg/qa-mohamed" "testOnly *MappingTest"
+::call sbt.bat "-Ddbcfg=pg/qa-mohamed" "testOnly *FileRepoListTest"
 ::call sbt.bat %EXTRA_OPTS% "testOnly *SchemeDumpTest"
 ::call sbt.bat "testOnly *ComponentTest"
 ::call sbt.bat "testOnly *ConnectTest"
