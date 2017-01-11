174d84cb73a647a327f913f6ad777614b40e562a
Ihar Hancharenka <iigoncharenko@mail.ru>
2017-01-11 20:16:18 +0300

    autocommit 2017-01-11 20:16:15

--- sbt/kkma/kkma-cur/src/test/scala/KKMAObserver.scala
+++ sbt/kkma/kkma-cur/src/test/scala/KKMAObserver.scala
@@ -3,9 +3,11 @@ import monix.reactive._
 import monix.execution.Ack
 import monix.execution.Ack.Continue
 
+import scala.collection.JavaConverters._
+
 import org.snu.ids.ha.ma.MorphemeAnalyzer
-//import org.snu.ids.ha.ma.Token;
-//import org.snu.ids.ha.ma.Tokenizer;
+import org.snu.ids.ha.ma.Tokenizer;
+import org.snu.ids.ha.ma.Token;
 
 import org.slf4j.Logger
 
@@ -14,15 +16,23 @@ class KKMAObserver(log: Logger) extends Observer.Sync[String] {
     private var line = 0
     private val ma = new MorphemeAnalyzer
 
+    def processTok(elem: String) {
+        val tokens = Tokenizer.tokenize(elem)
+        tokens.asScala.foreach { t: Token => log.info("{}", t.toString()) } // idx str charset
+    }
+
+    def processMA(elem: String) {
+        val mexp = ma.analyze(elem)
+        val bestMexp = ma.leaveJustBest(mexp)
+        val sentences = ma.divideToSentences(bestMexp)
+    }
+
     def onNext(elem: String): Ack = {
         line += 1
         log.info("processing {}: {}", line.toString, elem: Any)
 
-        val mexp = ma.analyze(elem)
-        val bestMexp = ma.leaveJustBest(mexp)
-        val sentences = ma.divideToSentences(bestMexp)
-
-        //Tokenizer.tokenize(elem)
+        //processMA(elem)
+        processTok(elem)
 
         Continue
     }
--- sbt/kkma/kkma-cur/src/test/scala/KKMASpec.scala
+++ sbt/kkma/kkma-cur/src/test/scala/KKMASpec.scala
@@ -19,8 +19,7 @@ import org.slf4j.LoggerFactory
 
 class KKMASpec extends FlatSpec {
     private val log = LoggerFactory.getLogger(getClass)
-    private val fileName = "lines" // "238"
-    //private val TEXTS = List("나라마다", "abc");
+    private val fileName = "970" // lines 238 970
 
     "KKMA" should "do some dumb assert" in {
         log.info("start")
