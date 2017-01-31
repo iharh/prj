14abaf7b91ef49d6ac60e5137b69ded935bd269b
Ihar Hancharenka <iigoncharenko@gmail.com>
2017-01-31 18:00:53 +0300

    m

--- sbt/dl4j/iter-model/build.sbt
+++ sbt/dl4j/iter-model/build.sbt
@@ -12,17 +12,19 @@ testOptions += Tests.Argument(TestFrameworks.JUnit, "-q", "-a")
 testOptions += Tests.Argument(TestFrameworks.ScalaTest, "-oDF")
 
 libraryDependencies ++= Seq(
-    "org.slf4j"                 % "slf4j-api"      % "1.7.22",
-    "org.slf4j"                 % "slf4j-log4j12"  % "1.7.22",
-    "log4j"                     % "log4j"          % "1.2.17",
+    "org.slf4j"                 % "slf4j-api"           % "1.7.22",
+    "org.slf4j"                 % "slf4j-log4j12"       % "1.7.22",
+    "log4j"                     % "log4j"               % "1.2.17",
+    //"org.apache.commons"        % "commons-compress"    % "1.13",
 
-    "org.deeplearning4j"        % "deeplearning4j-nlp" % "0.7.2" excludeAll(
+    "org.deeplearning4j"        % "deeplearning4j-nlp"  % "0.7.2" excludeAll(
         ExclusionRule(organization = "io.dropwizard"),
         ExclusionRule(organization = "ch.qos.logback"),
         ExclusionRule(organization = "com.typesafe.akka")
     ),
+    "com.jsuereth"              %% "scala-arm"          % "2.0",
     //
-    "org.scalatest"            %% "scalatest"      % "3.0.1"   % "test"
+    "org.scalatest"            %% "scalatest"           % "3.0.1"   % "test"
 )
 
 //libraryDependencies += "org.deeplearning4j" % "deeplearning4j-nlp" % "0.4-rc1.2" excludeAll(
--- sbt/dl4j/iter-model/src/test/scala/ModelSpec.scala
+++ sbt/dl4j/iter-model/src/test/scala/ModelSpec.scala
@@ -2,6 +2,8 @@ import org.scalatest._
 
 //import scala.collection.JavaConverters._
 
+import resource._
+
 import org.deeplearning4j.models.word2vec.Word2Vec
 import org.deeplearning4j.models.word2vec.VocabWord
 import org.deeplearning4j.models.embeddings.inmemory.InMemoryLookupTable
@@ -12,9 +14,16 @@ import org.nd4j.linalg.factory.Nd4j
 import org.nd4j.linalg.api.ndarray.INDArray
 import org.nd4j.linalg.ops.transforms.Transforms
 
+import org.apache.commons.compress.compressors.gzip.GzipUtils
+
 import java.io.File
 import java.io.InputStream
+import java.io.DataInputStream
+import java.io.FileInputStream
+import java.io.BufferedInputStream
 //import java.io.IOException
+
+import java.util.zip.GZIPInputStream;
 
 import org.slf4j.LoggerFactory
 
@@ -25,6 +34,79 @@ class ModelSpec extends FlatSpec with Matchers {
         log.info("start")
 
         log.info("end")
+    }
+}
+
+object ModelUtils {
+    private val MAX_SIZE = 50
+
+    def getBis(modelFile: File): BufferedInputStream = new BufferedInputStream(
+        if (GzipUtils.isCompressedFilename(modelFile.getName())) {
+            new GZIPInputStream(new FileInputStream(modelFile))
+        } else {
+            new FileInputStream(modelFile)
+        });
+
+    def readBinaryModel(modelFile: File): Word2Vec = { // throws NumberFormatException, IOException
+        var ret: Word2Vec = new Word2Vec() // new CustomWord2Vec();
+
+        for {
+            bis <- managed(getBis(modelFile))
+            dis <- managed(new DataInputStream(bis))
+        } {
+            val words:Int = Integer.parseInt(readString(dis))
+            val size: Int = Integer.parseInt(readString(dis))
+            var syn0: INDArray = Nd4j.create(words, size)
+            var cache: VocabCache[VocabWord] = new InMemoryLookupCache(false)
+            /*var lookupTable: InMemoryLookupTable[VocabWord] = new InMemoryLookupTable[VocabWord].Builder()
+                .cache(cache)
+                .vectorLength(size)
+                .build();
+
+            float [] vector = new float[size];
+            String word;
+
+            for (int i = 0; i < words; ++i) {
+                word = readString(dis);
+                if (cache.wordFrequency(word) > 0) {
+                    throw new IllegalArgumentException("Duplicate word has been found, word: " + word + ", lineNumber: " + i);
+                }
+
+                for (int j = 0; j < size; ++j) {
+                    vector[j] = readFloat(dis);
+                }
+
+                syn0.putRow(i, Transforms.unitVec(Nd4j.create(vector)));
+
+                cache.addWordToIndex(cache.numWords(), word);
+                cache.addToken(new VocabWord(1, word));
+                cache.putVocabWord(word);
+            }
+            lookupTable.setSyn0(syn0)
+            ret.setVocab(cache)
+            ret.setLookupTable(lookupTable)
+            */
+        }
+        ret
+    }
+
+    def readString(dis: DataInputStream): String = { // throws IOException
+        var bytes: Array[Byte] = new Array[Byte](MAX_SIZE)
+        var b: Byte = dis.readByte()
+        var i: Int = -1
+        var sb: StringBuilder = new StringBuilder()
+        while (b != 32 && b != 10) {
+            i = i + 1
+            bytes(i) = b
+            b = dis.readByte()
+            if (i == 49) {
+                sb.append(new String(bytes))
+                i = -1
+                bytes = new Array[Byte](MAX_SIZE)
+            }
+        }
+        sb.append(new String(bytes, 0, i + 1));
+        sb.toString()
     }
 
     def readFloat(is: InputStream): Float = { // throws IOException
