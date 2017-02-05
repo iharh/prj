47b7f907fadb2e6f8588c54c596c95c994fd8d3b
Ihar Hancharenka <iigoncharenko@gmail.com>
2017-02-05 17:00:25 +0300

    m

--- cargo/diesel_demo/src/lib.rs
+++ cargo/diesel_demo/src/lib.rs
@@ -11,6 +11,9 @@ use diesel::pg::PgConnection;
 use dotenv::dotenv;
 use std::env;
 
+use self::models::{Post, NewPost};
+
+
 pub fn establish_connection() -> PgConnection {
     dotenv().ok();
 
@@ -18,6 +21,19 @@ pub fn establish_connection() -> PgConnection {
         .expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url)
         .expect(&format!("Error connecting to {}", database_url))
+}
+
+pub fn create_post<'a>(conn: &PgConnection, title: &'a str, body: &'a str) -> Post {
+    use schema::posts;
+
+    let new_post = NewPost {
+        title: title,
+        body: body,
+    };
+
+    diesel::insert(&new_post).into(posts::table)
+        .get_result(conn)
+        .expect("Error saving new post")
 }
 
 #[cfg(test)]
