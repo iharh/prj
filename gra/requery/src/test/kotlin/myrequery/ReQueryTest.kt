4bd91a5c33a53f33e999e7d1034c8606b6ba785a
Ihar Hancharenka <iigoncharenko@gmail.com>
2017-03-16 10:32:08 +0300

    m

--- gra/requery/src/test/kotlin/myrequery/ReQueryTest.kt
+++ gra/requery/src/test/kotlin/myrequery/ReQueryTest.kt
@@ -80,6 +80,13 @@ class ReQueryTest : FunSpec() {
                 "abc" shouldBe p1.title
                 "abc def ggg" shouldBe p1.body
 
+                val p2 = PostEntity()
+                p2.id = 3
+                p2.title = "t3"
+                p2.body = "b3"
+
+                insert(p2)
+
                 //val 
             }
             //"".length shouldBe 0
