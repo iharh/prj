d438a7caa5aad541a17053491c85309fb4ed3314
Ihar Hancharenka <iigoncharenko@mail.ru>
2017-03-16 15:02:24 +0300

    m

--- gra/requery/src/test/kotlin/myrequery/ReQueryTest.kt
+++ gra/requery/src/test/kotlin/myrequery/ReQueryTest.kt
@@ -3,6 +3,7 @@ package myrequery
 import io.kotlintest.specs.FunSpec
 
 import org.postgresql.ds.PGSimpleDataSource
+import oracle.jdbc.pool.OracleDataSource
 
 //import io.requery.Persistable
 import io.requery.query.Result
@@ -11,7 +12,6 @@ import io.requery.sql.Platform
 import io.requery.sql.Configuration
 import io.requery.sql.ConfigurationBuilder
 import io.requery.sql.KotlinEntityDataStore
-import io.requery.sql.platform.PostgresSQL
 
 import io.requery.reactivex.KotlinReactiveEntityStore
 
@@ -51,22 +51,26 @@ interface WhereAndOr<E> : AndOr<WhereAndOr<E>>, SetGroupByOrderByLimit<E>
 
 class ReQueryTest : FunSpec() {
     companion object {
+	const val PRJ_ID = 2L // 20593L
 	const val PROP_EXPORT_TARGET_DIR = "SENT.EXPORT.TARGET_DIR"
 	const val PROP_EXPORT_TARGET_DIR_VAL = "\\\\freenas\\test1\\cmp-share1\\sent-exp"
     }
 
     init {
         test("String.length") {
-            val platform: Platform = PostgresSQL()
-            val dataSource = PGSimpleDataSource() // dataSourceClass.newInstance()
-            dataSource.setDatabaseName("postgres") // diesel_demo
-            dataSource.setUser("win_ss") // postgres
-            dataSource.setPassword("clb")
+            //val dataSource = PGSimpleDataSource() // dataSourceClass.newInstance()
+            //dataSource.setDatabaseName("postgres") // diesel_demo
+            //dataSource.setUser("win_ss") // postgres
+            //dataSource.setPassword("")
+
+            val dataSource = OracleDataSource() // dataSourceClass.newInstance()
+	    dataSource.setURL("")
+            dataSource.setUser("")
+            dataSource.setPassword("")
 
             val model: EntityModel = Models.DEFAULT
 
             val configuration: Configuration = ConfigurationBuilder(dataSource, model)
-                .setPlatform(platform)
                 .useDefaultLogging()
                 //.setEntityCache(new EmptyEntityCache())
                 .setWriteExecutor(Executors.newSingleThreadExecutor())
@@ -78,15 +82,15 @@ class ReQueryTest : FunSpec() {
             data.invoke {
 /*
                 val p_new = PropEntity()
-		p_new.projectId = 2L
+		p_new.projectId = PRJ_ID
 		p_new.name = PROP_EXPORT_TARGET_DIR
 		p_new.value = PROP_EXPORT_TARGET_DIR_VAL
 		insert(p_new) // insert
 */
-                val result = select(Prop::class) where((Prop::projectId eq 2L) and (Prop::name eq PROP_EXPORT_TARGET_DIR)) limit 1
+                val result = select(Prop::class) where((Prop::projectId eq PRJ_ID) and (Prop::name eq PROP_EXPORT_TARGET_DIR)) limit 1
                 val p1: Prop = result.get().first()
 
-                2L shouldBe p1.projectId
+                PRJ_ID shouldBe p1.projectId
                 PROP_EXPORT_TARGET_DIR shouldBe p1.name
                 PROP_EXPORT_TARGET_DIR_VAL shouldBe p1.value
             }
