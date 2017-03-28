027aa2515a190f9ba8d833b30f74b2488151d921
Ihar Hancharenka <iigoncharenko@mail.ru>
2017-03-28 17:34:40 +0300

    autocommit 2017-03-28 17:33:24

---
+++ gra/cld2test/src/main/java/My.java
@@ -0,0 +1,5 @@
+public class My {
+    static {
+        System.loadLibrary("cld2-windows-1.0.0"); // .dll
+    }
+}
---
+++ gra/cld2test/src/test/java/MyTest.java
@@ -0,0 +1,8 @@
+import junit.framework.TestCase;
+
+public class MyTest extends TestCase {
+    public void testMy() throws Exception {
+        My obj = new My();
+        assertNotNull(obj);
+    }
+}
