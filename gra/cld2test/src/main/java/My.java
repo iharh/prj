5d9069fb1b413eb959b828f83b3303f01cdaafe3
Ihar Hancharenka <iigoncharenko@mail.ru>
2017-03-30 13:59:53 +0300

    m

--- gra/cld2test/src/main/java/My.java
+++ gra/cld2test/src/main/java/My.java
@@ -1,5 +1,5 @@
 public class My {
     static {
-        System.loadLibrary("cld2-windows-1.0.0"); // .dll
+        // System.loadLibrary("cld2-windows-1.0.0"); // .dll
     }
 }
--- gra/cld2test/src/test/java/MyTest.java
+++ gra/cld2test/src/test/java/MyTest.java
@@ -1,8 +1,18 @@
 import junit.framework.TestCase;
 
+import jnr.ffi.LibraryLoader;
+
 public class MyTest extends TestCase {
+    public static interface LibCld2 {
+        int puts(String s);
+    }
+
     public void testMy() throws Exception {
-        My obj = new My();
-        assertNotNull(obj);
+        LibCld2 cld2 = LibraryLoader.create(LibCld2.class).load("cld2-windows-1.0.0");
+        assertNotNull(cld2);
+
+        assertEquals(1, cld2.puts(""));
+        //My obj = new My();
+        //assertNotNull(obj);
     }
 }
