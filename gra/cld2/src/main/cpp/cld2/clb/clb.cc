345e5b444ed66f762436190e4c974976386d5639
Ihar Hancharenka <iigoncharenko@mail.ru>
2017-03-31 15:45:47 +0300

    autocommit 2017-03-31 15:45:47

--- gra/cld2/src/main/cpp/cld2/clb/clb.cc
+++ gra/cld2/src/main/cpp/cld2/clb/clb.cc
@@ -1,3 +1,7 @@
+#include "encodings.h"
+#include "lang_script.h"
+#include "compact_lang_det_impl.h"
+
 #if defined(_MSC_VER)
     #define OVERRIDE override
     #define SEALED sealed
@@ -19,7 +23,47 @@
     #error Unsupported compiler
 #endif
 
+// generated_language.h - list of detected languages
 CLD_EXPORT
-int puts(char *in_str) {
-    return 2;
+int detectLangClb(const char *buffer) {
+    CLD2::Language detectedLangId = CLD2::UNKNOWN_LANGUAGE;
+
+    if (buffer != NULL) {
+        int buffer_length = strlen(buffer);
+
+        bool is_plain_text = true;
+
+        const char *tld_hint = "";
+        int encoding_hint = CLD2::UNKNOWN_ENCODING;
+        CLD2::Language language_hint = CLD2::UNKNOWN_LANGUAGE;
+        CLD2::CLDHints cldhints = {NULL, tld_hint, encoding_hint, language_hint};
+
+        bool allow_extended_lang = false;
+
+        CLD2::Language language3[3];
+        int percent3[3];
+        double normalized_score3[3];
+        int text_bytes;
+        int flags = 0;
+        CLD2::Language plus_one = CLD2::UNKNOWN_LANGUAGE;
+
+        bool is_reliable = false;
+
+        detectedLangId = CLD2::DetectLanguageSummaryV2(
+            buffer,
+            buffer_length,
+            is_plain_text,
+            &cldhints,
+            allow_extended_lang,
+            flags,
+            plus_one,
+            language3,
+            percent3,
+            normalized_score3,
+            NULL,
+            &text_bytes,
+            &is_reliable
+        );
+    }
+    return static_cast<int>(detectedLangId);
 }
--- gra/cld2test/src/test/java/MyTest.java
+++ gra/cld2test/src/test/java/MyTest.java
@@ -2,18 +2,21 @@ import junit.framework.TestCase;
 
 import jnr.ffi.LibraryLoader;
 
+import org.apache.commons.lang3.SystemUtils;
+
 public class MyTest extends TestCase {
+    private static final String CLD2_VER = "1.0.0";
+
     public static interface LibCld2 {
-        int puts(String s);
+        int detectLangClb(String text);
     }
 
     public void testMy() throws Exception {
-        final String libName = "cld2-linux-1.0.0"; // -windows-
+        final String libName = String.format("cld2-%s-%s", (SystemUtils.IS_OS_LINUX ? "linux" : "windows"), CLD2_VER);
         LibCld2 cld2 = LibraryLoader.create(LibCld2.class).load(libName);
         assertNotNull(cld2);
 
-        assertEquals(2, cld2.puts(""));
-        //My obj = new My();
-        //assertNotNull(obj);
+        //assertEquals(0, cld2.detectLangClb("I know and like so much my round table"));
+        assertEquals(4, cld2.detectLangClb("quatre petits enfants, trois filles malades"));
     }
 }
