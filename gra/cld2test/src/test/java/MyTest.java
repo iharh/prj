31a06f517010fd678cbc337e79b781c38d941ad9
Ihar Hancharenka <iigoncharenko@gmail.com>
2017-04-04 09:12:34 +0300

    m

--- gra/cld2test/src/test/java/MyTest.java
+++ gra/cld2test/src/test/java/MyTest.java
@@ -1,4 +1,7 @@
-import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,84 +25,63 @@ import java.util.Map;
 import java.util.List;
 import java.util.Arrays;
 
-import java.text.Normalizer;
-
 import java.io.File;
+import java.io.IOException;
 
 import static java.nio.charset.StandardCharsets.*;
 
 @Slf4j
-public class MyTest extends TestCase {
+public class MyTest {
     private static final String CLD2_VER = "1.0.0";
+    private static final boolean isLin = SystemUtils.IS_OS_LINUX;
+    private static final String libName = String.format("cld2-%s-%s", (isLin ? "linux" : "windows"), CLD2_VER);
+    private static final String inFileDir = (isLin ? "/data/wrk/clb" : "D:/clb/src") + "/spikes/iharh/ld/selected";
 
     @Encoding("UTF-8")
     public static interface LibCld2 {
         int detectLangClb(String text);
     }
 
-    private void processForLang(LibCld2 cld2, CSVFormat csvFormat, String langCode) throws Exception {
-        final String inFileDir = (SystemUtils.IS_OS_LINUX ? "/data/wrk/clb" : "D:/clb/src") + "/spikes/iharh/ld/selected";
+    private void processForLang(LibCld2 cld2, CSVFormat csvFormat, String langCode) throws IOException {
         final String inFileName = inFileDir + "/" + langCode + ".csv";
-
         final CSVParser csvParser = CSVParser.parse(new File(inFileName), UTF_8, csvFormat);
         for (final CSVRecord r : csvParser) {
             final String text = r.get(0);
-            //Normalizer.normalize(text, Normalizer.Form.NFD); // NFC - does not work
             int result = cld2.detectLangClb(text);
             //log.info("l: {}, code: {}, text: {}", text.length(), result, text);
         }
     }
 
-    //  metricRegistry.register("memory", new MemoryUsageGaugeSet());
-    //  metricRegistry.register("threads", new ThreadStatesGaugeSet());
-    //  metricRegistry.register("system", new PublicMetricSetAdapter(new SystemPublicMetrics()));
-
+    //FUTURE:
+    //https://github.com/dropwizard/metrics/blob/master/metrics-core/src/main/java/io/dropwizard/metrics/MetricName.java
+    //  metrics.register("threads", new ThreadStatesGaugeSet());
+    //  metrics.register("system", new PublicMetricSetAdapter(new SystemPublicMetrics()));
     //public interface MetricSet extends Metric {
     //    Map<MetricName, Metric> getMetrics();
     //}
 
-    //FUTURE:
-    //https://github.com/dropwizard/metrics/blob/master/metrics-core/src/main/java/io/dropwizard/metrics/MetricName.java
-
-    /*private void registerAll(String prefix, MetricSet metricSet, MetricRegistry registry) {
-        for (Map.Entry<String, Metric> entry : metricSet.getMetrics().entrySet()) {
-            if (entry.getValue() instanceof MetricSet) {
-                registerAll(prefix + "." + entry.getKey(), (MetricSet) entry.getValue(), registry);
-            } else {
-                registry.register(prefix + "." + entry.getKey(), entry.getValue());
-            }
-        }
-    }*/
-
-    public void doIter(LibCld2 cld2, CSVFormat csvFormat, MetricRegistry registry) throws Exception {
+    public void doIter(LibCld2 cld2, CSVFormat csvFormat, Map<String, Gauge> gauges) throws IOException {
         //List<String> langCodes = Arrays.asList("de", "ar", "en", "es", "fr", "it", "ja", "ko", "nl", "pt", "ru", "tr", "zh");
         List<String> langCodes = Arrays.asList("en");
         for (String langCode : langCodes) {
             processForLang(cld2, csvFormat, langCode);
         }
 
-        //assertEquals(0, cld2.detectLangClb("I know and like so much my round table"));
-        //assertEquals(4, cld2.detectLangClb("quatre petits enfants, trois filles malades"));
-        //26 - de
-        //
-
-        Map<String, Gauge> vals = registry.getGauges();
-
-        Object valInit = vals.get("memory.total.init").getValue();
-        Object valMax = vals.get("memory.total.max").getValue();
-        Object valUsed = vals.get("memory.total.used").getValue();
-        log.info("init: {}, max: {}, used: {}", valInit, valMax, valUsed);
+        Long valInit = (Long)gauges.get("memory.total.init").getValue();
+        Long valMax  = (Long)gauges.get("memory.total.max").getValue();
+        Long valUsed = (Long)gauges.get("memory.total.used").getValue();
+        log.info("{}, {}, {}", valInit, valMax, valUsed);
     }
 
+    @Test
     public void testMy() throws Exception {
-        log.info("start");
+        log.info("total.init, total.max, total.used");
 
-        final MetricRegistry registry = new MetricRegistry();
-        registry.register("memory", new MemoryUsageGaugeSet());
+        final MetricRegistry metrics = new MetricRegistry();
+        metrics.register("memory", new MemoryUsageGaugeSet());
 
-        final String libName = String.format("cld2-%s-%s", (SystemUtils.IS_OS_LINUX ? "linux" : "windows"), CLD2_VER);
         LibCld2 cld2 = LibraryLoader.create(LibCld2.class).load(libName);
-        assertNotNull(cld2);
+        assertThat(cld2, is(notNullValue()));
 
         final CSVFormat csvFormat = CSVFormat.DEFAULT
             .withIgnoreSurroundingSpaces(true)
@@ -107,10 +89,12 @@ public class MyTest extends TestCase {
             .withSkipHeaderRecord(true)
             .withHeader("TEXT");
 
-        for (int i = 0; i < 2; ++i) {
-            doIter(cld2, csvFormat, registry);
+        for (int i = 0; i < 3; ++i) {
+            doIter(cld2, csvFormat, metrics.getGauges());
         }
 
-        log.info("finish");
+        //26 - de?
+        //assertThat(cld2.detectLangClb("I know and like so much my round table"), is(0));
+        //assertEquals(cld2.detectLangClb("quatre petits enfants, trois filles malades"), is(4));
     }
 }
