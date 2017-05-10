f6b594e30660e643b3562788add0807f2d9ec1d1
Ihar Hancharenka <iigoncharenko@mail.ru>
2017-05-10 13:55:16 +0300

    m

--- gra/foma/src/main/c/s1/main.c
+++ gra/foma/src/main/c/s1/main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 
+#include "foma.h"
+
+void iface_load_stack(char *filename) {
+    struct fsm *net;
+    fsm_read_binary_handle fsrh;
+
+    if ((fsrh = fsm_read_binary_file_multiple_init(filename)) == NULL) {
+	fprintf(stderr, "%s: ", filename);
+        perror("File error");
+        return;
+    }
+    while ((net = fsm_read_binary_file_multiple(fsrh)) != NULL) {
+        printf("loaded fsm\n");
+        //stack_add(net);
+    }
+    return;
+}
+
+
 int
 main(void) {
-    printf("hello s1\n");
+    printf("start\n");
+    iface_load_stack("d:\\dev\prj\\gra\\foma\\morphind.bin");
+    printf("finish\n");
     return 0;
 }
