798709c41d045d9028af16739dc63463bc528189
Ihar Hancharenka <iigoncharenko@mail.ru>
2017-05-18 16:51:11 +0300

    autocommit 2017-05-18 16:51:09

---
+++ gra/clbfomatest/src/main/java/com/clarabridge/clbfoma/LibClbFoma.java
@@ -0,0 +1,9 @@
+package com.clarabridge.clbfoma;
+
+import jnr.ffi.annotations.Encoding;
+//import jnr.ffi.annotations.Synchronized;
+
+public interface LibClbFoma {
+    //@Encoding("UTF-8")
+    //int detectLangClb(String text);
+}
