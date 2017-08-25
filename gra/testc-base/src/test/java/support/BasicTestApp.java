37bf172854ea76f0d299ba9375284d598f35e4b8
Ihar Hancharenka <iigoncharenko@gmail.com>
2017-08-25 21:18:11 +0300

    m

---
+++ gra/testc-base/src/test/java/support/BasicTestApp.java
@@ -0,0 +1,25 @@
+package support;
+
+import feign.Feign;
+import feign.RequestLine;
+import feign.Response;
+import lombok.Getter;
+import org.testcontainers.containers.wait.HttpWaitStrategy;
+
+public class BasicTestApp extends GroovyTestApp<BasicTestApp> {
+
+    @Getter(lazy = true)
+    private final Client client = Feign.builder().target(Client.class, getURL());
+
+    public BasicTestApp() {
+        super("app.groovy");
+
+        withExposedPorts(4567);
+        setWaitStrategy(new HttpWaitStrategy().forPath("/hello/"));
+    }
+
+    public interface Client {
+        @RequestLine("GET /hello/")
+        Response getHello();
+    }
+}
