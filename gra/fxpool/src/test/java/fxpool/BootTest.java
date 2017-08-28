c7d337b613c3b6acce5af5cca07b0ad265e9ab82
Ihar Hancharenka <iigoncharenko@gmail.com>
2017-08-28 19:52:57 +0300

    m

--- gra/fxpool/src/test/java/fxpool/BootTest.java
+++ gra/fxpool/src/test/java/fxpool/BootTest.java
@@ -1,5 +1,6 @@
 package fxpool;
 
+import org.junit.ClassRule;
 import org.junit.Test;
 
 import org.testcontainers.containers.GenericContainer;
@@ -16,35 +17,31 @@ import java.util.concurrent.TimeoutException;
 import static org.rnorth.visibleassertions.VisibleAssertions.assertTrue;
 
 public class BootTest {
+    @ClassRule
+    public static JavaTestContainer javaTC = new JavaTestContainer();
 
     @Test
     public void simpleRecursiveFileTest() throws Exception { // TimeoutException
+        /*
         WaitingConsumer wait = new WaitingConsumer();
-
         final ToStringConsumer toStringConsumer = new ToStringConsumer();
-
-        //GenericContainer container = new JavaTestContainer() // JavaTestContainer
         GenericContainer container = new GenericContainer(
             new ImageFromDockerfile()
                 .withDockerfileFromBuilder(builder ->
                     builder.from("java:8")
                         .copy("/tmp/fxpool.jar", "fxpool.jar")
-                        //.cmd("cat /foo/test/resources/test-recursive-file.txt")
                         .cmd("java -jar fxpool.jar")
                         .build()
                 )
                 .withFileFromFile("/tmp/fxpool.jar", new File("./build/libs/fxpool.jar")));  // '.' is the project base directory
-
         container
             .withExposedPorts(18080) // -> 18080 at guest -> to random port at host-box
-            //.withStartupCheckStrategy(new OneShotStartupCheckStrategy())
-            //.withLogConsumer(wait.andThen(toStringConsumer))
-            //.withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger(script)));
             .setWaitStrategy(new HttpWaitStrategy().forPath("/v1/hello/"))
         ;
+        */
 
-        container.start();
-        wait.waitUntilEnd(360, TimeUnit.SECONDS); // need to wait here
+        //container.start();
+        //wait.waitUntilEnd(360, TimeUnit.SECONDS); // need to wait here
 
         //final String results = toStringConsumer.toUtf8String();
         //assertTrue("The container has a file that was copied in via a recursive copy", results.contains("Used for DirectoryTarResourceTest"));
--- gra/fxpool/src/test/java/fxpool/JavaTestContainer.java
+++ gra/fxpool/src/test/java/fxpool/JavaTestContainer.java
@@ -2,43 +2,39 @@ package fxpool;
 
 //import org.slf4j.LoggerFactory;
 
-import org.testcontainers.containers.BindMode;
+//import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.output.Slf4jLogConsumer;
+//import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.HttpWaitStrategy;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 
 import java.io.File;
 
 public class JavaTestContainer<SELF extends JavaTestContainer<SELF>> extends GenericContainer<SELF> {
 
-    public JavaTestContainer() { // (String script)
-
+    public JavaTestContainer() {
         super(new ImageFromDockerfile("myboot", false)
-            .withFileFromFile("fxpool.jar", new File("build/libs/fxpool.jar"))
-            .withDockerfileFromBuilder(builder -> {
-                builder
-                    .from("java:8")
-                    //.copy("service.war", "/usr/local/tomcat/webapps/my-service.war")
-                    .build();
-            }));
+            .withDockerfileFromBuilder(builder ->
+                builder.from("java:8")
+                    .copy("/tmp/fxpool.jar", "fxpool.jar")
+                    //.cmd("cat /foo/test/resources/test-recursive-file.txt")
+                    .cmd("java -jar fxpool.jar")
+                    .build()
+            )
+            .withFileFromFile("/tmp/fxpool.jar", new File("./build/libs/fxpool.jar")));  // '.' is the project base directory
 
         //withClasspathResourceMapping("agent.jar", "/agent.jar", BindMode.READ_ONLY);
         //withClasspathResourceMapping(script, "/app/app.groovy", BindMode.READ_ONLY);
 
-        // Cache Grapes
-        //addFileSystemBind(System.getProperty("user.home") + "/.groovy", "/root/.groovy/", BindMode.READ_WRITE);
-
-        //withEnv("JAVA_OPTS", "-javaagent:/agent.jar");
-
-        //withCommand("/opt/groovy/bin/groovy /app/app.groovy");
         withCommand("java -jar fxpool.jar");
 
-        //withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger(script)));
-
         withExposedPorts(18080);
+        setWaitStrategy(new HttpWaitStrategy().forPath("/v1/hello/"));
+        //withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger(script)));
     }
-
+/*
     public String getURL() {
         return "http://" + getContainerIpAddress() + ":" + getMappedPort(getExposedPorts().get(0)); // ? 18080
     }
+*/
 }
