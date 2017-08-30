8640787da8a0846ad7cae845f69f72d5e2dde8ce
Ihar Hancharenka <iigoncharenko@gmail.com>
2017-08-30 14:05:35 +0300

    m

--- gra/kts/build.gradle.kts
+++ gra/kts/build.gradle.kts
@@ -1,13 +1,26 @@
+import org.apache.commons.lang3.SystemUtils
+
+buildscript {
+    repositories {
+        jcenter()
+    }
+    dependencies {
+        classpath("org.apache.commons:commons-lang3:3.6")
+    }
+}
+
 apply {
     File("scripts").walk()
         .filter { !it.isDirectory() }
         .forEach {
-            // println("including " + it.toString())
-            from(it.toString())
+            from(it.toString()) // println("including " + it.toString())
         }
 }
 
 task("hello") {
     doLast {
+        println("host name: " + SystemUtils.OS_NAME)
+        //println("host name: " + SystemUtils.getHostName())
+        println("java home: " + SystemUtils.getJavaHome())
     }
 }
