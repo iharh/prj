95aa7e17c54b45ce526cf0705fe5b0fb6ca21acb
Ihar Hancharenka <iigoncharenko@mail.ru>
2017-09-01 15:49:17 +0300

    m

--- gra/cpphfst/src/main/cpp/main.cpp
+++ gra/cpphfst/src/main/cpp/main.cpp
@@ -6,25 +6,14 @@ int
 main(void)
 {
     std::cout << "start" << std::endl;
-    while (true) {
-        // int *pI = new int;
-        // delete pI;
-        //
-        FILE *f = ::fopen("d:\\download\\turkish.morph", "rb");
 
+    while (true) {
+        FILE *f = ::fopen("d:\\download\\turkish.morph", "rb");
         TransducerHeader header(f);
         TransducerAlphabet alphabet(f, header.symbol_count());
 
-        if (alphabet.get_state_size() == 0)
-        {   // if the state size is zero, there are no flag diacritics to handle
-            if (header.probe_flag(Weighted))
-            {
-                // no flags, weights, all analyses
-                TransducerW *pT = new TransducerW(f, header, alphabet);
-                // std::cout << "TransducerW created" << std::endl;
-                delete pT;
-            }
-        }
+        TransducerW *pT = new TransducerW(f, header, alphabet);
+        delete pT;
 
         ::fclose(f);
     }
