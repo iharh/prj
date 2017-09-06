0cc7f9966f570b435c595679005f8f2f144aef21
Ihar Hancharenka <iigoncharenko@mail.ru>
2017-09-06 13:44:00 +0300

    m

--- gra/kts/build.gradle.kts
+++ gra/kts/build.gradle.kts
@@ -1,9 +1,22 @@
+import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
+
 apply {
     File("scripts").walk()
         .filter { !it.isDirectory() && it.toString().endsWith(".gradle.kts") && !it.toString().endsWith(".swp") }
         .forEach {
             from(it.toString())
         }
+}
+
+plugins {
+    idea
+}
+
+idea {
+    project {
+        languageLevel = IdeaLanguageLevel(JavaVersion.VERSION_1_8) // "1.8"
+        vcs = "Git"
+    }
 }
 
 val hello by tasks.creating {
