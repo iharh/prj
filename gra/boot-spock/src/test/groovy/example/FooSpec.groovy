dd8cfea869c16f23bdaefad06222ca2ed4c6bda4
Ihar Hancharenka <iigoncharenko@gmail.com>
2017-09-18 10:36:43 +0300

    m

--- gra/boot-spock/src/test/groovy/example/FooSpec.groovy
+++ gra/boot-spock/src/test/groovy/example/FooSpec.groovy
@@ -5,31 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 
-import org.springframework.context.annotation.Import
 import org.springframework.http.HttpStatus
 
 import spock.lang.Specification
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 
-/**
- * An example of API integration test.
- *
- * @author Hidetake Iwata
- */
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-//@Import(IntegrationTestConfiguration)
 class FooSpec extends Specification {
     @Autowired
     TestRestTemplate restTemplate
 
-    //@Autowired
-    //ExternalApiClient client
-
     def '/foo should return world'() {
-        //given:
-        //1 * client.getDefault() >> new Hello('world')
-
         when:
         def entity = restTemplate.getForEntity('/foo', Hello)
 
