22fface495113b15306ece32d57a12b75a46f888
Ihar Hancharenka <iigoncharenko@gmail.com>
2017-10-20 12:17:06 +0300

    m

--- ca/le/06-generate-cert.sh
+++ ca/le/06-generate-cert.sh
@@ -3,4 +3,5 @@ set -e
 CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $CUR_DIR/env-vars.sh
 
+#java -jar acme_client.jar $OPT_LOG --command download-certificates -a $ACCOUNT_KEY -w $WORK_DIR --csr $CERT_CSR --cert-dir $CERT_DIR
 java -jar acme_client.jar $OPT_LOG --command generate-certificate -a $ACCOUNT_KEY -w $WORK_DIR --csr $CERT_CSR --cert-dir $CERT_DIR
