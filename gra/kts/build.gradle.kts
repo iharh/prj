f537a04965bb36a695594ff695694230fb99e9d3
Ihar Hancharenka <iigoncharenko@mail.ru>
2017-12-29 08:20:18 +0300

    autocommit 2017-12-29 08:20:17

--- gra/kts/build.gradle.kts
+++ gra/kts/build.gradle.kts
@@ -13,6 +13,7 @@ plugins {
 }
 
 val shlibcfg by configurations.creating
+val lpcfg by configurations.creating
 
 val nexusResolveUrl = rootProject.extra["nexus.repo.resolve"] as String
 
@@ -26,13 +27,22 @@ repositories {
         ivyPattern("${nexusResolveUrl}/[organisation]/[module]/[revision]/ivy.xml")
         artifactPattern("${nexusResolveUrl}/[organisation]/[module]/[revision]/[artifact](.[ext])")
     }
+
+    ivy (url = nexusResolveUrl) {
+        name = "cb_lp"
+        ivyPattern("${nexusResolveUrl}/[organisation]/[module]-linux/[revision]/ivy.xml")
+        artifactPattern("${nexusResolveUrl}/[organisation]/[module]-linux/[revision]/[artifact](.[ext])")
+    }
 }
 
 val fullFxVer = rootProject.extra["fx.version"] as String
+val fullLpEnVer = rootProject.extra["lp.english.version"] as String
 
 dependencies {
     // shared libs
     shlibcfg(group = "Clarabridge", name = "FX-windows-x64", version = fullFxVer, configuration = "publish")
+
+    lpcfg(group = "Clarabridge", name = "LP-English", version = fullLpEnVer, configuration = "publish")
 }
 
 idea {
@@ -58,8 +68,11 @@ val dirReport = "${dirBuild}/reports"
 
 val fileNameLPCfg = "${dirLP}/resources/config/config.xml"
 
+val dirTargetLp = "${dirTarget}/lp"
+val dirTargetLpResBase = "${dirTargetLp}/fx"
+
 tasks {
-    "cpSharedLibs" {
+    "cpFX" {
 	doLast {
 	    delete(dirFxLib)
             copy {
@@ -72,8 +85,26 @@ tasks {
             }
 	}
     }
+    "cpLP" {
+	doLast {
+	    delete(dirTargetLp)
+            copy {
+                from(lpcfg)
+                into(dirTargetLp)
+            }
+	    fileTree(dirTargetLp).files.forEach { artifact ->
+		println("Unpacking ${artifact.name}") // setup_en_7.3.2.0-7.3.2.0.zip
+		copy {
+		    from(zipTree(artifact))
+		    include("fx/*/**")
+		    into(dirTargetLp)
+		}
+	    }
+	}
+    }
     "benchEn" {
-	// dependsOn("cpSharedLibs")
+	// dependsOn("cpFX")
+	// dependsOn("cpLP")
 	// dependsOn("genLogCfg")
 	doLast {
 	    prepareBenchData(dirTargetData, fileNameArchivedDataSet)
@@ -96,7 +127,7 @@ tasks {
 		    systemProperty("java.library.path", dirFxLib)
 		    setArgs(listOf(
 			"-config", fileNameLPCfg,
-			"-resbasedir", dirResBase,
+			"-resbasedir", dirTargetLpResBase, // dirResBase
 			dirTargetData
 		    ))
 		}
