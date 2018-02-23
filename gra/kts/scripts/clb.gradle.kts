d54d2db3b874b233cd278833962699b238068073
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-02-23 08:42:38 +0300

    m

--- gra/kts/scripts/clb.gradle.kts
+++ gra/kts/scripts/clb.gradle.kts
@@ -1,8 +1,14 @@
+import org.apache.commons.lang3.SystemUtils
+
 import java.util.Properties
 
 val clbCfg by tasks.creating {
     doLast {
-	val instFolder = "D:/clb/inst"
+	val winInstFolder = "D:/clb/inst"
+	val linInstFolder = "/data/wrk/clb/inst"
+
+	val instFolder = if (SystemUtils.IS_OS_LINUX) linInstFolder else winInstFolder
+
 	val propFolder = "${instFolder}/configurer"
 	val propFileName = "${propFolder}/configurer-cmp.properties" 
 	val propFile = File(propFileName) // propertiesFile.exists()
