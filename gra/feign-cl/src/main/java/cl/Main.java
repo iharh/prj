6adf437dc391a0d2abe072a4c6de3ced71b3abb8
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-02-26 14:34:58 +0300

    m

--- gra/feign-cl/src/main/java/cl/Main.java
+++ gra/feign-cl/src/main/java/cl/Main.java
@@ -1,4 +1,19 @@
 package cl;
 
-public class Main {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+import lombok.extern.slf4j.Slf4j;
+
+@EnableAutoConfiguration
+@Slf4j
+public class Main implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        log.debug("start");
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
