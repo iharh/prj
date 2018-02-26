45cb5a00cbd155bd15e091b7e59b65d909be7a58
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-02-26 17:29:26 +0300

    m

--- gra/feign-cl/src/main/java/cl/FXResourceServiceConfiguration.java
+++ gra/feign-cl/src/main/java/cl/FXResourceServiceConfiguration.java
@@ -2,14 +2,21 @@ package cl;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 import feign.RequestInterceptor;
 import feign.auth.BasicAuthRequestInterceptor;
 
+import lombok.Getter;
+
 @Configuration
 public class FXResourceServiceConfiguration {
+    @Getter
+    @Value("${cmp.pwd}")
+    private String pwd;
+
     @Bean
     RequestInterceptor feignRequestInterceptor() {
-        return new BasicAuthRequestInterceptor("admin", "admin");
+        return new BasicAuthRequestInterceptor("admin", pwd);
     }
 }
