19d6f90da5f9f82ce67b234b56a83519a398e9c0
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-03-13 14:15:15 +0300

    m

--- gra/native/icu4c/src/main/cpp/main.cpp
+++ gra/native/icu4c/src/main/cpp/main.cpp
@@ -1,8 +1,26 @@
+#include <unicode/unistr.h>
+
 #include <iostream>
+#include <locale>
+
+namespace std
+{
+    inline std::wostream &operator <<(std::wostream &stream, const UnicodeString &s)
+    {
+        for (int i = 0; i < s.length(); ++i)
+            stream.put(s.charAt(i));
+        return stream;
+    }
+}
 
 int
 main(void)
 {
-    std::wcout << L"Hello world" << std::endl;
+    //std::locale::global (std::locale ("en_US.UTF-8"));
+    setlocale(LC_CTYPE, "en_US.UTF-8");
+
+    UnicodeString orig(UNICODE_STRING_SIMPLE("abc"));
+
+    std::wcout << L"Hello привет world lội " << orig << L" !" << std::endl;
     return 0;
 }
---
+++ gra/native/icu4c/t.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/env bash
+build/exe/main/main
