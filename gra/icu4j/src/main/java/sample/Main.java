3ac9c90fb16074900f2e412213ecbe3153aac40a
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-03-13 15:58:37 +0300

    m

--- gra/icu4j/src/main/java/sample/Main.java
+++ gra/icu4j/src/main/java/sample/Main.java
@@ -1,7 +1,29 @@
 package sample;
 
-public class Main {
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+
+import com.ibm.icu.text.Normalizer;
+//import com.ibm.icu.text.Normalizer2;
+
+public class Main implements CommandLineRunner {
+    private static void compDecomp(String orig) {
+        String compF = Normalizer.compose(orig, false);
+        String compT = Normalizer.compose(orig, true);
+        String decompF = Normalizer.decompose(orig, false);
+        String decompT = Normalizer.decompose(orig, true);
+        System.out.println(orig + " " + compF + " " + compT + " " + decompF + " " + decompT);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        compDecomp("ộ");
+    }
+
     public static void main(String [] args) {
-        System.out.println("Hello world!");
+        SpringApplication app = new SpringApplication(Main.class);
+        app.setBannerMode(Banner.Mode.OFF); // OFF LOG
+        SpringApplication.run(Main.class, args);
     }
 }
---
+++ gra/icu4j/t.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/env bash
+java -Dspring.main.banner-mode=off -jar build/libs/icu4j.jar
