e4d7ff8d85447826491730f4476650e7479563dd
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-03-13 15:14:13 +0300

    m

--- gra/native/icu4c/src/main/cpp/main.cpp
+++ gra/native/icu4c/src/main/cpp/main.cpp
@@ -1,4 +1,5 @@
 #include <unicode/unistr.h>
+#include <unicode/normlzr.h>
 
 #include <iostream>
 #include <locale>
@@ -13,14 +14,35 @@ namespace std
     }
 }
 
+void
+compDecomp(const UnicodeString &orig)
+{
+    UnicodeString composed;
+    UErrorCode status = U_ZERO_ERROR;
+    Normalizer::compose(orig, TRUE, 0, composed, status);
+    if (U_FAILURE(status))
+    {
+        std::wcout << L"Normalizer::compose failure detected" << std::endl;
+    }
+    status = U_ZERO_ERROR;
+    UnicodeString decomposed;
+    Normalizer::decompose(orig, TRUE, 0, decomposed, status);
+    if (U_FAILURE(status))
+    {
+        std::wcout << L"Normalizer::decompose failure detected" << std::endl;
+    }
+
+    std::wcout << orig << L" " << composed << L" " << decomposed << std::endl;
+}
+
 int
 main(void)
 {
     //std::locale::global (std::locale ("en_US.UTF-8"));
     setlocale(LC_CTYPE, "en_US.UTF-8");
 
-    UnicodeString orig(UNICODE_STRING_SIMPLE("abc"));
-
-    std::wcout << L"Hello привет world lội " << orig << L" !" << std::endl;
+    //compDecomp(UNICODE_STRING_SIMPLE("loi"));
+    //compDecomp(UNICODE_STRING_SIMPLE("lội"));
+    compDecomp(UNICODE_STRING_SIMPLE("ộ"));
     return 0;
 }
