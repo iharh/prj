f97d297d734696e6bc5bf0d7f24f01e926730501
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-03-20 16:30:40 +0300

    m

--- dub/clb/utils/src/clbutils.d
+++ dub/clb/utils/src/clbutils.d
@@ -12,14 +12,23 @@ import core.time;
 
 //extern(C) void test();
 
-void wsdlProject(string data) {
+void wsdl(string name, string data) {
     auto rq = Request();
     rq.verbosity = 2;
     rq.timeout = 5.minutes;
     rq.authenticator = new BasicAuthentication("admin", "admin");
-    auto rs = rq.post("http://localhost:18080/cbapi/project?wsdl", data, "text/xml");
+    string url = format("http://localhost:18080/cbapi/%s?wsdl", name);
+    auto rs = rq.post(url, data, "text/xml");
     writeln(format("code: %d", rs.code));
     writeln(format("resp: %s", rs.responseBody));
+}
+
+void wsdlProject(string data) {
+    wsdl("project", data);
+}
+
+void wsdlRealtime(string data) {
+    wsdl("realtime", data);
 }
 
 void prjCreate(string langId, string prjName) {
@@ -33,5 +42,5 @@ void pmvd(string prjName, string verbatimText) {
     app.compileHTMLDietFile!("pmvd.dt", prjName, verbatimText);
 
     writeln(format("compiled to: %s", app.data));
-    // wsdlRealtime(app.data);
+    wsdlRealtime(app.data);
 }
