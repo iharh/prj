e558cfd16e48eb95a0f07aff6dc9509b45f4b4fe
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-03-21 09:36:58 +0300

    m

--- dub/clb/src/app.d
+++ dub/clb/src/app.d
@@ -5,7 +5,7 @@ import clbutils;
 
 void main() {
     string langId = "en";
-    string prjSuffix = "0int";
+    string prjSuffix = "0ext";
     string prjName = langId ~ prjSuffix;
     //prjCreate(langId, prjName);
     //prjCreate(langId, langId ~ "0ext");
