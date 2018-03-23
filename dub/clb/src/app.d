75c2964c58da403b337d3d7e04a8e648035b6e9f
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-03-23 17:27:37 +0300

    m

--- dub/clb/src/app.d
+++ dub/clb/src/app.d
@@ -1,19 +1,35 @@
-import std.stdio;
 import std.format;
+import std.range;
+import std.stdio;
 
 import clbutils;
 
+void createProjects(string langId) {
+    prjCreate(langId, langId ~ "0int");
+    prjCreate(langId, langId ~ "0ext");
+}
+
+void uploadByLine(string prjName, string fileName) {
+    auto f = File(fileName);
+    auto lines = f.byLineCopy();
+    foreach(lineNum, line; lines.enumerate(1)) {
+        writeln(format("line %d: %s", lineNum, line));
+        pmvd(prjName, true, line);
+    }
+}
+
 void main() {
-    string langId = "en";
+    auto langId = "en";
+    // createProjects(langId);
+
     string prjSuffix = "0ext";
-    //string prjSuffix = "0int";
     string prjName = langId ~ prjSuffix;
-    //prjCreate(langId, prjName);
-    //prjCreate(langId, langId ~ "0ext");
+    uploadByLine(prjName, langId ~ "100.txt");
+/*
 
     bool isSave = false;
     string responseBody = pmvd(prjName, isSave, "I like my round dreamliner");
-
     auto respFile = File("result.xml", "w");
     respFile.write(responseBody);
+*/
 }
