785eb59ddb49051c9b7cf9c63ad7c14e65da2091
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-04-13 11:53:08 +0300

    m

--- dub/clb/src/app.d
+++ dub/clb/src/app.d
@@ -34,8 +34,8 @@ int main(string[] args) {
     auto prjNameInt = langId ~ "0int";
     auto prjNameExt = langId ~ "0ext";
     auto prjNames = [ prjNameInt , prjNameExt ];
-    auto dataFileName = langId ~ "1.txt";
-    //auto dataFileName = langId ~ "100.txt";
+    //auto dataFileName = langId ~ "1.txt";
+    auto dataFileName = langId ~ "100.txt";
 
     Options options;
     try {
