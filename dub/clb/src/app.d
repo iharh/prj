f45e33f932dc756b33a4ed20a8f8f652ba9093a6
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-04-27 11:18:05 +0300

    m

--- dub/clb/src/app.d
+++ dub/clb/src/app.d
@@ -93,7 +93,7 @@ int main(string[] args) {
         if (options.upload) {
             bool isSave = true;
             auto lines = File(dataFileName).byLineCopy();
-            foreach (lineNum, line; lines.enumerate(2)) {
+            foreach (lineNum, line; lines.enumerate(3)) {
                 //foreach (prjName; prjNames) {
                     auto prjName = prjNameExt;
                     writeln(format("line %d: prj: %s ...", lineNum, prjName));
