f72f7d31bfe0d5f92b5a9e9a42e3fc541ac3a32d
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-05-10 13:59:43 +0300

    m

--- js/pug1/webpack.config.js
+++ js/pug1/webpack.config.js
@@ -3,10 +3,14 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     // mode: 'production',
     mode: 'development',
-    entry: './src/app.js',
+    entry: './src/app.tsx',
     output: {
         path: __dirname + "/public", // need to be absolute
         filename: 'bundle.js'
+    },
+    devtool: "source-map",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"]
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -21,6 +25,16 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader"
+            },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                test: /\.js$/,
+                enforce: "pre",
+                loader: "source-map-loader"
+            },
+            {
                 test : /\.pug$/,
                 loader: 'pug-loader'
             },
@@ -29,5 +43,13 @@ module.exports = {
                 test: /\.js/
             }
         ]
-    }
-}
+    },
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    //externals: {
+    //    "react": "React",
+    //    "react-dom": "ReactDOM"
+    //},
+};
