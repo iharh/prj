9148263577853bcc48a61d16cada1aae4166ed3d
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-05-13 15:06:33 +0300

    m

--- js/pug1/src/app.tsx
+++ js/pug1/src/app.tsx
@@ -2,8 +2,15 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { Hello } from "./components/Hello";
+import { TaskGrid } from "./components/TaskGrid";
 
 ReactDOM.render(
     <Hello compiler="TypeScript" framework="React" />,
     document.getElementById("root")
 );
+ReactDOM.render(
+    <TaskGrid/>,
+    document.getElementById("rootsvg")
+);
+    //  g(class="row")
+    //    rect(class="square" x="1" y="1" width="50" height="50" style="stroke: rgb(34, 34, 34); fill: white;")
---
+++ js/pug1/src/components/TaskGrid.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export const TaskGrid = () => <g><rect x={1} y={1} width={50} height={50} style={{stroke:"rgb(34, 34, 34)", fill:"white"}}/></g>;
+
+// className="square"
+//<rect x=1 y=1 width=50 height=50 style="stroke: rgb(34, 34, 34); fill: white;"/>
+// stroke: black
