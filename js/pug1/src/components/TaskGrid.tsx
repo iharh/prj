d28ffbe779f06e6d5bbc2b46622a08590f12b473
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-05-16 21:09:46 +0300

    m

--- js/pug1/src/components/TaskGrid.tsx
+++ js/pug1/src/components/TaskGrid.tsx
@@ -1,6 +1,16 @@
 import * as React from "react";
 
-export const TaskGrid = () => <g><rect x={1} y={1} width={50} height={50} style={{stroke:"rgb(34, 34, 34)", fill:"white"}}/></g>;
+export const TaskGrid = () => <g style={{stroke:"black", fill:"white"}} >
+    <g transform="translate(0 0)">>
+        <rect x={1} width={50} height={50} />
+        <rect x={51} width={50} height={50} />
+    </g>
+    <g transform="translate(0 50)">
+        <rect x={1} width={50} height={50} />
+        <rect x={51} width={50} height={50} />
+        <rect x={101} width={50} height={50} />
+    </g>
+</g>;
 
 // className="square"
 //<rect x=1 y=1 width=50 height=50 style="stroke: rgb(34, 34, 34); fill: white;"/>
