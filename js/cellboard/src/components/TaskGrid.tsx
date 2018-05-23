3b0c6cd228d5501700b93c13aa98e88d681afdfb
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-05-23 15:12:42 +0300

    m

--- js/cellboard/src/components/TaskGrid.tsx
+++ js/cellboard/src/components/TaskGrid.tsx
@@ -1,19 +1,20 @@
 import * as React from "react";
 import { GridBox } from "./GridBox";
+import { Range } from "immutable";
 
 export interface TaskGridProps { cellSize: number; }
 
 // style={{stroke:"black", fill:"white"}}
 const taskGridStyle= {stroke:"black", fill:"white"};
 
-export const TaskGrid = (props: TaskGridProps) => <g style={taskGridStyle}>
-    <g transform="translate(0 0)">>
-        <GridBox x={1} cellSize={props.cellSize} />
-        <GridBox x={51} cellSize={props.cellSize} />
-    </g>
-    <g transform="translate(0 50)">
-        <GridBox x={1} cellSize={props.cellSize} />
-        <GridBox x={51} cellSize={props.cellSize} />
-        <GridBox x={101} cellSize={props.cellSize} />
-    </g>
-</g>;
+export const TaskGrid = (props: TaskGridProps) => {
+    let gridBoxMapper = (i: number) => <GridBox x={1+i*props.cellSize} cellSize={props.cellSize} />;
+    return <g style={taskGridStyle}>
+        <g transform="translate(0 1)">
+            {Range(0, 2).map(gridBoxMapper)}
+        </g>
+        <g transform="translate(0 51)">
+            {[0, 1, 2].map(gridBoxMapper)}
+        </g>
+    </g>;
+};
