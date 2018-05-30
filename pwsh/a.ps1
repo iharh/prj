8448a4d348b995934ca9a31b7ac70c6fb9121a57
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-05-30 09:48:34 +0300

    m

--- pwsh/a.ps1
+++ pwsh/a.ps1
@@ -1,4 +1,13 @@
 #!/usr/bin/env pwsh
 # run with: TERM=xterm <this-script>.ps1
-$var1="val1"
-echo "This is $var1 value."
+$newPath=$env:PATH
+
+# dot-folders are automatically filtered out without -force
+get-childitem -path "/data/wrk/bin/cmd" -recurse | where psiscontainer |
+foreach-object {
+    $dirname = $_.fullname
+    $newPath="${newPath}:${dirname}"
+    echo "dirname: ${dirname}"
+}
+$env:PATH=$newPath
+echo "PATH: ${env:PATH}"
