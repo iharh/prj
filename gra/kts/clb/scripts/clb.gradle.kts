5acd1da06e88aafe68cc39d1106fc6cc65004a5a
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-06-05 14:48:33 +0300

    hi

--- gra/kts/clb/scripts/clb.gradle.kts
+++ gra/kts/clb/scripts/clb.gradle.kts
@@ -37,7 +37,7 @@ val clbCfg by tasks.creating {
 	val serviceCustomProps = Properties()
 	serviceCustomProps.setProperty("ehcache.cache.name", "ehcache.dev")
 	serviceCustomProps.setProperty("ehcache.alerts.cache.name", "ehcache-alerts.dev")
-        listOf("ar", "en", "es").forEach {
+        listOf("ar", "en", "es", "hi").forEach {
             serviceCustomProps.setProperty("feign.fxservice.${it}.host", "http://${fxsvcHost}:8080")
         }
 	propServiceCustomFile.outputStream().use { serviceCustomProps.store(it, null) }
