e3e579df1cc97dcad8bd6f809d6588b74ebcde91
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-06-13 13:23:19 +0300

    m

--- gra/boot/osvercheck/src/main/java/cli/Main.java
+++ gra/boot/osvercheck/src/main/java/cli/Main.java
@@ -10,10 +10,16 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Main implements CommandLineRunner {
+
+    private boolean isWinServer2008() {
+        final String osName = SystemUtils.OS_NAME;
+        return osName != null && osName.startsWith("Windows Server 2008");
+    }
+
     @Override
     public void run(String... args) throws Exception {
-        boolean isWinServer2008 = SystemUtils.IS_OS_WINDOWS_2008;
-        log.warn("isWinServer2008: {}", (isWinServer2008 ? "true" : "false"));
+        log.warn("os.name: {}, os.version: {}", SystemUtils.OS_NAME, SystemUtils.OS_VERSION);
+        log.warn("isWinServer2008: {}", (isWinServer2008() ? "true" : "false"));
     }
 
     public static void main(String [] args) {
