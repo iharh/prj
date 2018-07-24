0bfe7208abeb7f442aa8a8b299d252345992f5c2
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-07-24 16:11:53 +0300

    m

--- gra/boot/caffeine/src/main/java/cl/Main.java
+++ gra/boot/caffeine/src/main/java/cl/Main.java
@@ -3,14 +3,25 @@ package cl;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 
+import org.springframework.cache.CacheManager;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Main implements CommandLineRunner {
+
+    private final CacheManager cacheManager;
+    // private final ResourceService resourceService;
+
+    // @Autowired
+    public Main(CacheManager cacheManager/*, ResourceService resourceService*/) {
+        this.cacheManager = cacheManager;
+        // this.resourceService = resourceService;
+    }
+
     @Override
     public void run(String... args) throws Exception {
-        log.info("start");
-        log.debug("some debug");
+        log.info("cacheManager null: {}", (cacheManager == null ? "true" : "false"));
     }
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
