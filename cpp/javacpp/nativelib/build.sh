679b4d7f474510aea48d780b15e9468439f8c810
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-09-20 11:36:47 +0300

    m

--- cpp/javacpp/nativelib/build.sh
+++ cpp/javacpp/nativelib/build.sh
@@ -2,4 +2,4 @@
 JAVACPP_JAR=~/Downloads/javacpp-1.4.2.jar
 javac -cp $JAVACPP_JAR NativeLibrary.java
 java -jar $JAVACPP_JAR NativeLibrary -nodelete
-java -cp ~/Downloads/javacpp-1.4.2.jar:. NativeLibrary
+java -cp $JAVACPP_JAR:. NativeLibrary
