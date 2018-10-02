acbec5a70a0a46fe7d9f6870661c1f97d0c8f96b
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-10-02 20:30:20 +0300

    m

---
+++ gra/ben-test/src/test/java/ben/BenTests.java
@@ -0,0 +1,14 @@
+package ben;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class BenTests {
+    @Test
+    void justAnExample() throws Exception {
+        assertTrue(true);
+    }
+}
