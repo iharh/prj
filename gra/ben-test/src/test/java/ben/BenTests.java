51eda16d54029da458be72e512d43e85c92510d1
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-10-04 08:37:31 +0300

    m

--- gra/ben-test/src/test/java/ben/BenTests.java
+++ gra/ben-test/src/test/java/ben/BenTests.java
@@ -33,6 +33,9 @@ public class BenTests {
     private static final int REQ_SOCKET_TIMEOUT = 20000;
     private static final int REQ_CONNECT_TIMEOUT = 20000;
 
+    private static final String SAMPLE_TEXT1 = "খালিদ  জামিলকে দেখে মনে হচ্ছে খাঁচাবন্দি ‘বাঘ’..!";
+    private static final String EXP_RES1 = "CgYIARoCICAKYggCEAcaFeCmnOCmvuCmruCmv+CmsuCmleCnhyJFCg/gppzgpr7gpq7gpr/gprISMv7//////////wHz//////////8B4P//////////Adf//////////wHT//////////8BCgUQDhoBIAojIiEKCeCmpuCnh+CmlhIU9v//////////AcH//////////wEKNyI1Cgngpqbgp4fgppYSKPb//////////wHH//////////8ByP//////////Ac3//////////wEKUwgCEA8aDOCmpuCnh+CmluCnhyI/Cgngpqbgp4fgppYSMvb//////////wHH//////////8ByP//////////Ac7//////////wHi//////////8BCgUQExoBIApNCAIQFBoJ4Kau4Kao4KeHIjwKBuCmruCmqBIy////////////Ad7//////////wHc//////////8B1///////////AdH//////////wEKBRAXGgEgCjEiLwoD4Ka5Eij2//////////8Bx///////////Acb//////////wHN//////////8BClAIAhAYGg/gprngpprgp43gppvgp4ciOQoD4Ka5EjL2//////////8Bx///////////Acb//////////wHO//////////8B4v//////////AQoFEB0aASAKCggBEB4aBCDigJgKUAgCECoaCeCmrOCmvuCmmCI/Cgngpqzgpr7gppgSMv///////////wHg//////////8B2///////////Adf//////////wHU//////////8BCgcQLRoD4oCZChgIAhAuGgEuIg8KAS4SCr///////////wEKGAgCEC8aAS4iDwoBLhIKv///////////AQ==";
+
     private static URI createURI(String text) throws IOException{
         try {
             return new URIBuilder()
@@ -55,10 +58,10 @@ public class BenTests {
     }
 
     @Test
-    void justAnExample() throws Exception {
+    public void justAnExample() throws Exception {
         RequestConfig lttRequestConfig = createRequestConfig();
         assertNotNull(lttRequestConfig);
-        URI lttURI = createURI("খালিদ  জামিলকে দেখে মনে হচ্ছে খাঁচাবন্দি ‘বাঘ’..!");
+        URI lttURI = createURI(SAMPLE_TEXT1);
         assertNotNull(lttURI);
 
         try (CloseableHttpClient lttHttpClient = HttpClients.createDefault()) {
@@ -73,11 +76,13 @@ public class BenTests {
                 try (ByteArrayOutputStream bos = new ByteArrayOutputStream((int)lttRespLen)) { // specifying direct size impacts performance a lot
                     lttResponseEntity.writeTo(bos); 
 
-                    String expectedRes = "CgYIARoCICAKYggCEAcaFeCmnOCmvuCmruCmv+CmsuCmleCnhyJFCg/gppzgpr7gpq7gpr/gprISMv7//////////wHz//////////8B4P//////////Adf//////////wHT//////////8BCgUQDhoBIAojIiEKCeCmpuCnh+CmlhIU9v//////////AcH//////////wEKNyI1Cgngpqbgp4fgppYSKPb//////////wHH//////////8ByP//////////Ac3//////////wEKUwgCEA8aDOCmpuCnh+CmluCnhyI/Cgngpqbgp4fgppYSMvb//////////wHH//////////8ByP//////////Ac7//////////wHi//////////8BCgUQExoBIApNCAIQFBoJ4Kau4Kao4KeHIjwKBuCmruCmqBIy////////////Ad7//////////wHc//////////8B1///////////AdH//////////wEKBRAXGgEgCjEiLwoD4Ka5Eij2//////////8Bx///////////Acb//////////wHN//////////8BClAIAhAYGg/gprngpprgp43gppvgp4ciOQoD4Ka5EjL2//////////8Bx///////////Acb//////////wHO//////////8B4v//////////AQoFEB0aASAKCggBEB4aBCDigJgKUAgCECoaCeCmrOCmvuCmmCI/Cgngpqzgpr7gppgSMv///////////wHg//////////8B2///////////Adf//////////wHU//////////8BCgcQLRoD4oCZChgIAhAuGgEuIg8KAS4SCr///////////wEKGAgCEC8aAS4iDwoBLhIKv///////////AQ==";
-                    assertEquals(expectedRes, bos.toString()); // bos.toByteArray()
+                    assertEquals(EXP_RES1, bos.toString()); // bos.toByteArray()
                 } 
                 EntityUtils.consume(lttResponseEntity); // we can use toString(resEntity) also
             }
         }
+    }
+
+    private void dumpRes(String data) {
     }
 }
