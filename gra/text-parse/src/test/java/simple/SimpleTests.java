a22eb91e80419928bae0c06656d57636d2e32c48
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-11-02 15:50:57 +0300

    m

---
+++ gra/text-parse/src/test/java/simple/SimpleTests.java
@@ -0,0 +1,16 @@
+package simple;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
+
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SimpleTests {
+    @Test
+    public void test1() throws Exception {
+        String srcText = "নেই।নেই।";
+        assertNotNull(srcText);
+    }
+}
