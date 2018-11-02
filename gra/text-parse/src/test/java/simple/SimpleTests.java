ff75fd62749dfb4c7f778ede0277596fae24ee46
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-11-02 15:56:18 +0300

    m

--- gra/text-parse/src/test/java/simple/SimpleTests.java
+++ gra/text-parse/src/test/java/simple/SimpleTests.java
@@ -7,10 +7,27 @@ import org.junit.jupiter.api.Disabled;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SimpleTests {
+    private String punctuationSeparators = "।?!";
+    private String plainTextBreakPattern = "\\r?\\n(?:\\r?\\n|(?=(?:[ \\t]+(?:\\-|[ ]{2,}))))";
+
+    private Pattern exclusivePattern;
+
+    private Pattern makePattern(String patternText) {
+        if (patternText != null && patternText.length() > 0) {
+            return Pattern.compile(patternText);
+        }
+        return null;
+    }
+
     @Test
     public void test1() throws Exception {
         String srcText = "নেই।নেই।";
-        assertNotNull(srcText);
+
+        exclusivePattern = makePattern("[" + punctuationSeparators + "]|(" + plainTextBreakPattern + ")");
+        assertNotNull(exclusivePattern);
     }
 }
