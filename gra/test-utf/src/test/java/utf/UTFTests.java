7b9889d09c59a5e9c680d5e37b1177db32e3eb06
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-11-08 21:32:07 +0300

    m

--- gra/test-utf/src/test/java/utf/UTFTests.java
+++ gra/test-utf/src/test/java/utf/UTFTests.java
@@ -3,6 +3,10 @@ package utf;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Disabled;
 
+import org.apache.commons.codec.binary.Hex;
+
+import static java.nio.charset.StandardCharsets.*;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UTFTests {
@@ -14,6 +18,7 @@ public class UTFTests {
 
     @Test
     public void test1() throws Exception {
+        dbg(Hex.encodeHexString("123".getBytes(UTF_8)));
         assertThat(true).isEqualTo(true);
     }
 }
