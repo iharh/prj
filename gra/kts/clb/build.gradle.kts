10656f086b618dbd5cd950e560be64a8a4e5c6cb
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-11-13 20:52:35 +0300

    m

--- gra/kts/clb/build.gradle.kts
+++ gra/kts/clb/build.gradle.kts
@@ -77,71 +77,71 @@ val dirTargetLpResBase = "${dirTargetLp}/fx"
 
 tasks {
     "cpFX" {
-	doLast {
-	    delete(dirFxLib)
+        doLast {
+            delete(dirFxLib)
             copy {
                 from(shlibcfg) {
-		    rename { filename -> filename.replace("-${fullFxVer}", "") }
-		}
-		include("*.dll") // TODO: win/lin
-		include("*.jar")
+                    rename { filename -> filename.replace("-${fullFxVer}", "") }
+                }
+                include("*.dll") // TODO: win/lin
+                include("*.jar")
                 into(dirFxLib)
             }
-	}
+        }
     }
     "cpLP" {
-	doLast {
-	    delete(dirTargetLp)
+        doLast {
+            delete(dirTargetLp)
             copy {
                 from(lpcfg)
                 into(dirTargetLp)
             }
-	    fileTree(dirTargetLp).files.forEach { artifact ->
-		println("Unpacking ${artifact.name}") // setup_en_7.3.2.0-7.3.2.0.zip
-		copy {
-		    from(zipTree(artifact))
-		    include("fx/*/**")
-		    into(dirTargetLp)
-		}
-	    }
-	}
+            fileTree(dirTargetLp).files.forEach { artifact ->
+                println("Unpacking ${artifact.name}") // setup_en_7.3.2.0-7.3.2.0.zip
+                copy {
+                    from(zipTree(artifact))
+                    include("fx/*/**")
+                    into(dirTargetLp)
+                }
+            }
+        }
     }
     "bench" {
-	// dependsOn("cpFX")
-	// dependsOn("cpLP")
-	doLast {
-	    prepareBenchData(dirTargetData, fileNameArchivedDataSet)
-	    val cpJfx = fileTree(dirFxLib) {
-		include("**/*.jar")
-	    }
-	    //cpJfx.forEach { println(it) }
+        // dependsOn("cpFX")
+        // dependsOn("cpLP")
+        doLast {
+            prepareBenchData(dirTargetData, fileNameArchivedDataSet)
+            val cpJfx = fileTree(dirFxLib) {
+                include("**/*.jar")
+            }
+            //cpJfx.forEach { println(it) }
 
-	    //delete(dirReport)
-	    //mkdir(dirReport)
-	    for (i in 1..15) {
-		doGenLogCfg("log4j-bench.jade", dirFxLib, "${dirReport}/${i}")
-		javaexec {
-		    main = "com.clarabridge.fx.Main"
-		    classpath = cpJfx // shlibcfg
-		    setWorkingDir(dirFxLib)
-		    setIgnoreExitValue(true)
-		    // systemProperty("log4j.debug", "true")
-		    systemProperty("log4j.configuration", "file:/${dirFxLib}/log4j.xml")
-		    systemProperty("java.library.path", dirFxLib)
-		    setArgs(listOf(
-			"-config", fileNameLPCfg,
-			"-resbasedir", dirTargetLpResBase, // dirResBase
-			dirTargetData
-		    ))
-		}
-	    }
-	}
+            //delete(dirReport)
+            //mkdir(dirReport)
+            for (i in 1..15) {
+                doGenLogCfg("log4j-bench.jade", dirFxLib, "${dirReport}/${i}")
+                javaexec {
+                    main = "com.clarabridge.fx.Main"
+                    classpath = cpJfx // shlibcfg
+                    setWorkingDir(dirFxLib)
+                    setIgnoreExitValue(true)
+                    // systemProperty("log4j.debug", "true")
+                    systemProperty("log4j.configuration", "file:/${dirFxLib}/log4j.xml")
+                    systemProperty("java.library.path", dirFxLib)
+                    setArgs(listOf(
+                        "-config", fileNameLPCfg,
+                        "-resbasedir", dirTargetLpResBase, // dirResBase
+                        dirTargetData
+                    ))
+                }
+            }
+        }
     }
     "stat" {
-	doLast {
+        doLast {
             val userHome = System.getProperty("user.home") // user.dir
-	    val dirNameReportsBase = "${userHome}/Downloads"
-	    fxBenchParseGenCSV(0, 'a', "${dirNameReportsBase}/reports", arrayOf(
+            val dirNameReportsBase = "${userHome}/Downloads"
+            fxBenchParseGenCSV(0, 'a', "${dirNameReportsBase}/reports", arrayOf(
                 "7.5.0.0-7.3.0.0",
                 "7.5.1.0-7.3.1.0",
                 "7.5.2.0-7.3.2.0",
@@ -151,23 +151,23 @@ tasks {
                 "7.5.2.14-7.3.2.0",
                 "7.5.2.15-7.3.2.0"
             ))
-	    fxBenchParseGenCSV(1, 'v', "${dirNameReportsBase}/reports-big", arrayOf(
+            fxBenchParseGenCSV(1, 'v', "${dirNameReportsBase}/reports-big", arrayOf(
                 "7.5.2.10-7.3.2.0",
                 "7.5.2.11-7.3.2.0",
                 "7.5.2.13-7.3.2.0",
                 "7.5.2.14-7.3.2.0",
                 "7.5.2.15-7.3.2.1"
             ))
-	}
+        }
     }
     "hello" {
-	doLast {
-	    println("fullFxVer: ${fullFxVer}")
-	    for (i in 1..3) {
-		println(listOf("hello", "world", i))
-	    }
-	    mySuperFun(dirReport)
-	}
+        doLast {
+            println("fullFxVer: ${fullFxVer}")
+            for (i in 1..3) {
+                println(listOf("hello", "world", i))
+            }
+            mySuperFun(dirReport)
+        }
     }
 }
 
