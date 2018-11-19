72186c2d839f75e55cbb65c039e1650061967ed1
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-11-19 10:40:04 +0300

    m

--- gra/kts/clb/build.gradle.kts
+++ gra/kts/clb/build.gradle.kts
@@ -75,99 +75,103 @@ val fileNameLPCfg = "${dirLP}/resources/config/config.xml"
 val dirTargetLp = "${dirTarget}/lp"
 val dirTargetLpResBase = "${dirTargetLp}/fx"
 
-tasks {
-    "cpFX" {
-        doLast {
-            delete(dirFxLib)
-            copy {
-                from(shlibcfg) {
-                    rename { filename -> filename.replace("-${fullFxVer}", "") }
-                }
-                include("*.dll") // TODO: win/lin
-                include("*.jar")
-                into(dirFxLib)
+task("hello") {
+    doLast {
+        println("fullFxVer: ${fullFxVer}")
+        for (i in 1..3) {
+            println(listOf("hello", "world", i))
+        }
+        mySuperFun(dirReport)
+    }
+}
+
+task("cpFX") {
+    doLast {
+        delete(dirFxLib)
+        copy {
+            from(shlibcfg) {
+                rename { filename -> filename.replace("-${fullFxVer}", "") }
             }
+            include("*.dll") // TODO: win/lin
+            include("*.jar")
+            into(dirFxLib)
         }
     }
-    "cpLP" {
-        doLast {
-            delete(dirTargetLp)
+}
+
+task("cpLP") {
+    doLast {
+        delete(dirTargetLp)
+        copy {
+            from(lpcfg)
+            into(dirTargetLp)
+        }
+        fileTree(dirTargetLp).files.forEach { artifact ->
+            println("Unpacking ${artifact.name}") // setup_en_7.3.2.0-7.3.2.0.zip
             copy {
-                from(lpcfg)
+                from(zipTree(artifact))
+                include("fx/*/**")
                 into(dirTargetLp)
             }
-            fileTree(dirTargetLp).files.forEach { artifact ->
-                println("Unpacking ${artifact.name}") // setup_en_7.3.2.0-7.3.2.0.zip
-                copy {
-                    from(zipTree(artifact))
-                    include("fx/*/**")
-                    into(dirTargetLp)
-                }
-            }
         }
     }
-    "bench" {
-        // dependsOn("cpFX")
-        // dependsOn("cpLP")
-        doLast {
-            prepareBenchData(dirTargetData, fileNameArchivedDataSet)
-            val cpJfx = fileTree(dirFxLib) {
-                include("**/*.jar")
-            }
-            //cpJfx.forEach { println(it) }
+}
 
-            //delete(dirReport)
-            //mkdir(dirReport)
-            for (i in 1..15) {
-                doGenLogCfg("log4j-bench.jade", dirFxLib, "${dirReport}/${i}")
-                javaexec {
-                    main = "com.clarabridge.fx.Main"
-                    classpath = cpJfx // shlibcfg
-                    setWorkingDir(dirFxLib)
-                    setIgnoreExitValue(true)
-                    // systemProperty("log4j.debug", "true")
-                    systemProperty("log4j.configuration", "file:/${dirFxLib}/log4j.xml")
-                    systemProperty("java.library.path", dirFxLib)
-                    setArgs(listOf(
-                        "-config", fileNameLPCfg,
-                        "-resbasedir", dirTargetLpResBase, // dirResBase
-                        dirTargetData
-                    ))
-                }
+/*
+task("bench" {
+    // dependsOn("cpFX")
+    // dependsOn("cpLP")
+    doLast {
+        prepareBenchData(dirTargetData, fileNameArchivedDataSet)
+        val cpJfx = fileTree(dirFxLib) {
+            include("** /*.jar")
+        }
+        //cpJfx.forEach { println(it) }
+
+        //delete(dirReport)
+        //mkdir(dirReport)
+        for (i in 1..15) {
+            doGenLogCfg("log4j-bench.jade", dirFxLib, "${dirReport}/${i}")
+            javaexec {
+                main = "com.clarabridge.fx.Main"
+                classpath = cpJfx // shlibcfg
+                setWorkingDir(dirFxLib)
+                setIgnoreExitValue(true)
+                // systemProperty("log4j.debug", "true")
+                systemProperty("log4j.configuration", "file:/${dirFxLib}/log4j.xml")
+                systemProperty("java.library.path", dirFxLib)
+                setArgs(listOf(
+                    "-config", fileNameLPCfg,
+                    "-resbasedir", dirTargetLpResBase, // dirResBase
+                    dirTargetData
+                ))
             }
         }
     }
-    "stat" {
-        doLast {
-            val userHome = System.getProperty("user.home") // user.dir
-            val dirNameReportsBase = "${userHome}/Downloads"
-            fxBenchParseGenCSV(0, 'a', "${dirNameReportsBase}/reports", arrayOf(
-                "7.5.0.0-7.3.0.0",
-                "7.5.1.0-7.3.1.0",
-                "7.5.2.0-7.3.2.0",
-                "7.5.2.10-7.3.2.0",
-                "7.5.2.11-7.3.2.0",
-                "7.5.2.13-7.3.2.0",
-                "7.5.2.14-7.3.2.0",
-                "7.5.2.15-7.3.2.0"
-            ))
-            fxBenchParseGenCSV(1, 'v', "${dirNameReportsBase}/reports-big", arrayOf(
-                "7.5.2.10-7.3.2.0",
-                "7.5.2.11-7.3.2.0",
-                "7.5.2.13-7.3.2.0",
-                "7.5.2.14-7.3.2.0",
-                "7.5.2.15-7.3.2.1"
-            ))
-        }
-    }
-    "hello" {
-        doLast {
-            println("fullFxVer: ${fullFxVer}")
-            for (i in 1..3) {
-                println(listOf("hello", "world", i))
-            }
-            mySuperFun(dirReport)
-        }
+}
+*/
+
+task("stat") {
+    doLast {
+        val userHome = System.getProperty("user.home") // user.dir
+        val dirNameReportsBase = "${userHome}/Downloads"
+        fxBenchParseGenCSV(0, 'a', "${dirNameReportsBase}/reports", arrayOf(
+            "7.5.0.0-7.3.0.0",
+            "7.5.1.0-7.3.1.0",
+            "7.5.2.0-7.3.2.0",
+            "7.5.2.10-7.3.2.0",
+            "7.5.2.11-7.3.2.0",
+            "7.5.2.13-7.3.2.0",
+            "7.5.2.14-7.3.2.0",
+            "7.5.2.15-7.3.2.0"
+        ))
+        fxBenchParseGenCSV(1, 'v', "${dirNameReportsBase}/reports-big", arrayOf(
+            "7.5.2.10-7.3.2.0",
+            "7.5.2.11-7.3.2.0",
+            "7.5.2.13-7.3.2.0",
+            "7.5.2.14-7.3.2.0",
+            "7.5.2.15-7.3.2.1"
+        ))
     }
 }
 
--- gra/kts/clb/scripts/clb.gradle.kts
+++ gra/kts/clb/scripts/clb.gradle.kts
@@ -5,7 +5,7 @@ import java.util.Properties
 val clbCfg by tasks.creating {
     doLast {
         // is it possible to do this with val ?
-	var instFolder = "" 
+        var instFolder = "" 
         var fxsvcHost = ""
         if (SystemUtils.IS_OS_LINUX) {
             instFolder = "/data/wrk/clb/inst"
@@ -16,30 +16,30 @@ val clbCfg by tasks.creating {
             fxsvcHost = "192.168.235.101"
         }
 
-	val propFolder = "${instFolder}/configurer"
+        val propFolder = "${instFolder}/configurer"
         val serverFolder = "${instFolder}/server"
         val tempFolder = "${serverFolder}/temp"
 
-	val propConfigurerCmpFileName = "${propFolder}/configurer-cmp.properties" 
-	val propConfigurerCmpFile = File(propConfigurerCmpFileName) // propConfigurerCmpFile.exists()
-	val configurerCmpProps = Properties()
-	propConfigurerCmpFile.inputStream().use { configurerCmpProps.load(it) }
-	configurerCmpProps.setProperty("directory.install", instFolder)
+        val propConfigurerCmpFileName = "${propFolder}/configurer-cmp.properties" 
+        val propConfigurerCmpFile = File(propConfigurerCmpFileName) // propConfigurerCmpFile.exists()
+        val configurerCmpProps = Properties()
+        propConfigurerCmpFile.inputStream().use { configurerCmpProps.load(it) }
+        configurerCmpProps.setProperty("directory.install", instFolder)
 
-	val tempConfigurerCmpFile = File("${propFolder}/temp-configurer-cmp.properties")
-	tempConfigurerCmpFile.outputStream().use { configurerCmpProps.store(it, null) }
-	tempConfigurerCmpFile.copyTo(propConfigurerCmpFile, true)
-	tempConfigurerCmpFile.delete()
+        val tempConfigurerCmpFile = File("${propFolder}/temp-configurer-cmp.properties")
+        tempConfigurerCmpFile.outputStream().use { configurerCmpProps.store(it, null) }
+        tempConfigurerCmpFile.copyTo(propConfigurerCmpFile, true)
+        tempConfigurerCmpFile.delete()
 
         val serverConfFolder = "${serverFolder}/conf"
-	val propServiceCustomFileName = "${serverConfFolder}/properties-service-custom.properties" 
-	val propServiceCustomFile = File(propServiceCustomFileName)
-	val serviceCustomProps = Properties()
-	serviceCustomProps.setProperty("ehcache.cache.name", "ehcache.dev")
-	serviceCustomProps.setProperty("ehcache.alerts.cache.name", "ehcache-alerts.dev")
+        val propServiceCustomFileName = "${serverConfFolder}/properties-service-custom.properties" 
+        val propServiceCustomFile = File(propServiceCustomFileName)
+        val serviceCustomProps = Properties()
+        serviceCustomProps.setProperty("ehcache.cache.name", "ehcache.dev")
+        serviceCustomProps.setProperty("ehcache.alerts.cache.name", "ehcache-alerts.dev")
         listOf("ar", "de", "en", "es", "fr", "hi", "it", "pt").forEach {
             serviceCustomProps.setProperty("feign.fxservice.${it}.host", "http://${fxsvcHost}:8080")
         }
-	propServiceCustomFile.outputStream().use { serviceCustomProps.store(it, null) }
+        propServiceCustomFile.outputStream().use { serviceCustomProps.store(it, null) }
     }
 }
