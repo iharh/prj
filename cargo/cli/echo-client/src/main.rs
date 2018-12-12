93cf98b9423481aa09e2c7ab707ff293b4a88f7b
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-12-12 16:54:57 +0300

    m

--- cargo/cli/echo-client/src/main.rs
+++ cargo/cli/echo-client/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+//use std::{fs, io, path::Path};
+
+// io::Error
+// 
+
+fn main() -> Result<(), Box<std::error::Error>> {
+    // let client = reqwest::Client::new();
+    let _client = reqwest::Client::builder()
+        .build()?;
+
+    //let respBody = client.get("http://localhost:8000/hello")
+    //    ?.text()?;
+
+    //println!("respBody = {:?}", respBody);
+    println!("hello!");
+    Ok(())
 }
