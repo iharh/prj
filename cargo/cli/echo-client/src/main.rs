db287409ec67f6fb07251592087c6be1c12e9c1f
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-12-12 17:32:48 +0300

    m

--- cargo/cli/echo-client/src/main.rs
+++ cargo/cli/echo-client/src/main.rs
@@ -1,17 +1,23 @@
 //use std::{fs, io, path::Path};
 
+// https://doc.rust-lang.org/stable/std/boxed/
+
 // io::Error
-// 
+// https://rust-lang-nursery.github.io/edition-guide/rust-2018/error-handling-and-panics/question-mark-in-main-and-tests.html
+
+// https://github.com/seanmonstar/reqwest/blob/master/examples/simple.rs
+// cargo run --example simple
+
+fn single() -> Result<(), Box<std::error::Error>> {
+    // let mut resp = reqwest::get("http://localhost:8000/hello")?;
+    let resp = reqwest::get("http://localhost:8091/analyze?text=abc")?;
+    assert!(resp.status() == 200);
+    // std::io::copy(&mut resp, &mut std::io::stdout())?;
+    // println!("respBody = {:?}", respBody);
+    Ok(())
+}
 
 fn main() -> Result<(), Box<std::error::Error>> {
-    // let client = reqwest::Client::new();
-    let _client = reqwest::Client::builder()
-        .build()?;
-
-    //let respBody = client.get("http://localhost:8000/hello")
-    //    ?.text()?;
-
-    //println!("respBody = {:?}", respBody);
-    println!("hello!");
-    Ok(())
+    // let client = reqwest::Client::builder().build()?;
+    single()
 }
