2d421324bd979ae31a567a460e7b7a1d08949f69
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-12-14 11:38:55 +0300

    m

--- cargo/search-client/src/main.rs
+++ cargo/search-client/src/main.rs
@@ -1,20 +1,20 @@
 // `error_chain!` can recurse deeply
 #![recursion_limit = "1024"]
 
-// use reqwest::{Client, StatusCode};
-// use std::collections::HashMap;
-// use std::io::{BufRead, BufReader};
+use reqwest::{Client, StatusCode};
+use std::collections::HashMap;
+use std::io::{BufRead, BufReader};
+use std::time::Duration;
 use std::thread;
 use std::vec;
-//use std::{fs, io, path::Path};
 
+extern crate crossbeam_channel as channel;
 #[macro_use]
 extern crate error_chain;
 
-extern crate crossbeam_channel as channel;
-
 pub struct Work {
-    work_id: usize,
+    iter_id: usize,
+    line_id: usize,
     text: String,
 }
 
@@ -29,6 +29,7 @@ mod errors {
             IO(std::io::Error);
             Reqwest(reqwest::Error);
             ChannelSendError(channel::SendError<super::Message>);
+            TryRecvError(channel::TryRecvError);
         }
     }
 }
@@ -42,94 +43,86 @@ struct Worker {
 
 type ResT<T> = errors::Result<T>;
 
-/*
-fn process_request(client: &Client, text: &str) -> ResT<()> {
-    // http://localhost:8091/analyze
-    //let resp = client.get("http://localhost:8080/analyze")
-    //    .query(&[("text", text)])
-    //    .send()?;
-
-    let mut map = HashMap::new();
-    map.insert("text", text);
-
-    let resp = client.post("http://localhost:8080/process")
-        .json(&map)
-        .send()?;
-
-    assert!(resp.status() == StatusCode::OK);
-    // std::io::copy(&mut resp, &mut std::io::stdout())?;
-    // println!("respBody = {:?}", respBody);
-    Ok(())
-}
-
-fn process_file(client: &Client) -> ResT<()> {
-    let file = std::fs::File::open("input.txt")?;
-    let reader = BufReader::new(file);
-    for (_index, line) in reader.lines().enumerate() {
-        let line = line?;
-        // println!("{}. {}", index + 1, line);
-        // println!("line: {}", index + 1);
-        process_request(client, &line)?
-    }
-    Ok(())
-}
-*/
-
 impl Worker {
-    fn run(self) { // mut self
-        while let Some(_work) = self.get_work() { // mut work
-            // println!("thread: {} wordId: {}, text: {}", self.thread_id, work.work_id, work.text);
+    fn run(self) -> ResT<()> { // mut self
+        let client = Client::new();
+        while let Some(work) = self.get_work()? { // mut work
+            self.process_request(&client, &work.text)?;
         }
+        Ok(())
     }
 
-    fn get_work(&self) -> Option<Work> { // &mut self
-        match self.rx.try_recv() {
-            Ok(Message::Work(work)) => {
-                println!("thread: {} wordId: {}, text: {}", self.thread_id, work.work_id, work.text);
-                return Some(work);
-            }
-            Ok(Message::Quit) => {
-                println!("thread: {} quit recieved", self.thread_id);
-                return None;
-            }
-            Err(_) => {
-                println!("thread: {} err recieved", self.thread_id);
-                return None;
+    fn process_request(&self, client: &Client, text: &str) -> ResT<()> {
+        // http://localhost:8091/analyze
+        //let resp = client.get("http://localhost:8080/analyze")
+        //    .query(&[("text", text)])
+        //    .send()?;
+
+        let mut map = HashMap::new();
+        map.insert("text", text);
+
+        let resp = client.post("http://localhost:8080/process")
+            .json(&map)
+            .send()?;
+
+        assert!(resp.status() == StatusCode::OK);
+        Ok(())
+    }
+
+    fn get_work(&self) -> ResT<Option<Work>> { // &mut self
+        loop {
+            match self.rx.try_recv() {
+                Ok(Message::Work(work)) => {
+                    println!("thread: {} iter: {} line: {}", self.thread_id, work.iter_id, work.line_id);
+                    return Ok(Some(work));
+                }
+                Ok(Message::Quit) => {
+                    println!("thread: {} quit", self.thread_id);
+                    return Ok(None);
+                }
+                Err(channel::TryRecvError::Empty) => {
+                    thread::sleep(Duration::from_millis(100));
+                }
+                Err(channel::TryRecvError::Disconnected) => {
+                    return Ok(None);
+                }
             }
         }
     }
 }
 
 fn main() -> ResT<()> {
-    let num_threads = 4;
-    let (tx, rx) = channel::unbounded::<Message>();
+    let num_threads = 32;
+    let num_iters = 100000;
+    let (tx, rx) = channel::bounded::<Message>(num_threads * num_threads);
+    let mut handles = vec![];
 
-    // let client = Client::new();
-    for index in 0..50 {
-        // println!("iter: {}", index);
-        // process_file(&client)?;
-        tx.send(Message::Work(
-            Work { work_id: index, text: format!("item{}", index), }
-        ))?;
+    for idx in 0..num_threads {
+        let worker = Worker {
+            thread_id: idx,
+            rx: rx.clone()
+        };
+        handles.push(thread::spawn(|| worker.run()));
+    }
 
+    for iter in 0..num_iters {
+        let file = std::fs::File::open("input.txt")?;
+        let reader = BufReader::new(&file);
+        for (index, line) in reader.lines().enumerate() {
+            let line = line?;
+            tx.send(Message::Work(
+                Work { iter_id: iter, line_id: index, text: line, }
+            ))?;
+        }
     }
 
     for _ in 0..num_threads {
         tx.send(Message::Quit)?;
     }
 
-    let mut handles = vec![];
-    for idx in 0..num_threads {
-        let worker = Worker {
-            thread_id: idx, rx: rx.clone()
-        };
-        handles.push(thread::spawn(|| worker.run()));
-    }
-
     for handle in handles {
-        handle.join().unwrap();
+        handle.join().unwrap()?;
     }
-
     Ok(())
 }
 
