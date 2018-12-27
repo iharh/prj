a440f2722ad913612e1dd35bf209947260fd4cd0
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-12-27 15:03:21 +0300

    m

---
+++ gra/ant-int/core/jmy/test/com/myorg/my/JmyTest.java
@@ -0,0 +1,12 @@
+package com.myorg.my;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class JmyTest {
+    @Test
+    public void test1() throws Exception {
+        assertTrue(true);
+    }
+}
