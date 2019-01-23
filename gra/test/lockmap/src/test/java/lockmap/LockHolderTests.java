1e01416caf93cf070ddd0604145f0cef8fe61e16
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-01-23 17:54:34 +0300

    m

--- gra/test/lockmap/src/test/java/lockmap/LockHolderTests.java
+++ gra/test/lockmap/src/test/java/lockmap/LockHolderTests.java
@@ -2,12 +2,15 @@ package lockmap;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.IntStream;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LockHolderTests {
+    private static final int ITERATION_CNT = 1000000;
 
     @AllArgsConstructor
     @Getter
@@ -23,7 +26,15 @@ public class LockHolderTests {
     void testLock() throws Exception {
         final LockHolderImpl holder = new LockHolderImpl();
 
-        assertThat(true).isEqualTo(true);
+        NonAtomicInt nonAtomicInt = new NonAtomicInt(0);
+
+        IntStream.range(0, ITERATION_CNT)
+             .boxed()
+             .parallel()
+             .forEach(i -> holder.withWriteLockAcquired(0, nonAtomicInt::increment));
+
+        Thread.sleep(1000);
+
+        assertThat(nonAtomicInt.getValue()).isEqualTo(ITERATION_CNT);
     }
 }
-
