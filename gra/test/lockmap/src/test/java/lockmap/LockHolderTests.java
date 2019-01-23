dd8515e8f50c4a04d2b2c00287dd03b25e44096d
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-01-23 17:49:00 +0300

    m

--- gra/test/lockmap/src/test/java/lockmap/LockHolderTests.java
+++ gra/test/lockmap/src/test/java/lockmap/LockHolderTests.java
@@ -2,11 +2,27 @@ package lockmap;
 
 import org.junit.jupiter.api.Test;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LockHolderTests {
+
+    @AllArgsConstructor
+    @Getter
+    public class NonAtomicInt {
+        private int value;
+
+        public void increment() {
+            this.value++;
+        }
+    }
+
     @Test
     void testLock() throws Exception {
+        final LockHolderImpl holder = new LockHolderImpl();
+
         assertThat(true).isEqualTo(true);
     }
 }
