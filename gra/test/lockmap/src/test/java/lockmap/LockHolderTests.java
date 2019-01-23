c802e6add0d9505d57d6b06828b1f5ab3340fef5
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-01-23 17:31:53 +0300

    m

---
+++ gra/test/lockmap/src/test/java/lockmap/LockHolderTests.java
@@ -0,0 +1,13 @@
+package lockmap;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LockHolderTests {
+    @Test
+    void testLock() throws Exception {
+        assertThat(true).isEqualTo(true);
+    }
+}
+
