88fe55a073b1a15d5c4008fbb50a780a890dda0e
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-03-04 21:34:39 +0300

    m

--- gra/boot/restsecure/src/main/java/my/config/MySecurityConfig.java
+++ gra/boot/restsecure/src/main/java/my/config/MySecurityConfig.java
@@ -1,10 +1,27 @@
 package my.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
@@ -12,13 +29,11 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+            .addFilterBefore(myFilter(), BasicAuthenticationFilter.class)
             .authorizeRequests()
-                //.antMatchers("/v1/**").permitAll()
-                // .anyRequest().hasRole("USER")
                 .anyRequest().authenticated()
                 .and()
             .httpBasic()
-            //.formLogin().loginPage("/login").failureUrl("/login-error")
         ;
     }
 
@@ -27,5 +42,26 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
         auth
             .inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER");
+    }
+
+    private Filter myFilter() {
+        return new OncePerRequestFilter() {
+            @Override
+            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+                    throws IOException, ServletException {
+
+                logger.info("!!! myFilter enter !!!");
+
+                UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken("user", "password");
+                authRequest.setDetails(this.authenticationDetailsSource.buildDetails(request));
+
+                Authentication authResult = this.authenticationManager.authenticate(authRequest);
+
+                SecurityContextHolder.getContext().setAuthentication(authResult);
+                // TODO: catch AuthenticationException
+                
+                chain.doFilter(request, response);
+            }
+        };
     }
 }
