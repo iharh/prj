691ba493cd41a9ea70bd9a35f99ef3e21b93c34d
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-03-12 13:24:05 +0300

    m

--- gra/test/test-utf/src/test/java/utf/UTFTests.java
+++ gra/test/test-utf/src/test/java/utf/UTFTests.java
@@ -5,6 +5,13 @@ import org.junit.jupiter.api.Disabled;
 
 import org.apache.commons.codec.binary.Hex;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.FileSystems;
+
 import static java.nio.charset.StandardCharsets.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,6 +25,27 @@ public class UTFTests {
 
     @Test
     public void test1() throws Exception {
+        int num = 0;
+        System.out.println("start");
+
+        Path path = FileSystems.getDefault().getPath("/home/iharh/Downloads", "a.txt");
+        try (BufferedReader br = Files.newBufferedReader(path, Charset.forName("windows-1251"))) {
+            String line = null;
+            do {
+                line = br.readLine();
+                ++num;
+                System.out.println(num);
+                if (line != null) {
+                    System.out.println(line);
+                }
+            } while (line != null);
+        }
+        assertThat(num).isGreaterThan(0);
+        // assertThat(num).isEqualTo(10241);
+    }
+
+    @Disabled
+    public void test2() throws Exception {
         String src = "1😌2";
 
         int i = 0;
