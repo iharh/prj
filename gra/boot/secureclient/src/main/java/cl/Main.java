03d1625dc98fb6f312310f954d08318ade807325
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-03-25 22:09:45 +0300

    m

--- gra/boot/secureclient/src/main/java/cl/Main.java
+++ gra/boot/secureclient/src/main/java/cl/Main.java
@@ -15,24 +15,26 @@ import org.springframework.security.oauth2.client.token.grant.code.Authorization
 
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
-@ComponentScan({"cl"})
+//@ComponentScan({"cl"})
 @EnableOAuth2Client
 public class Main implements CommandLineRunner {
+    /*
     @Autowired
     OAuth2ClientContext oauth2ClientContext;
+
+    @Bean
+    @ConfigurationProperties("localas.client")
+    public AuthorizationCodeResourceDetails localAuthServer() {
+        return new AuthorizationCodeResourceDetails();
+    }
+    */
 
     @Override
     public void run(String... args) throws Exception {
         System.out.println("starting ...");
-        OAuth2RestTemplate localAuthServerTemplate = new OAuth2RestTemplate(localAuthServer(), oauth2ClientContext);
+        // OAuth2RestTemplate localAuthServerTemplate = new OAuth2RestTemplate(localAuthServer(), oauth2ClientContext);
         System.out.println("done.");
         
-    }
-
-    @Bean
-    @ConfigurationProperties("localAuthServer.client")
-    public AuthorizationCodeResourceDetails localAuthServer() {
-        return new AuthorizationCodeResourceDetails();
     }
 
     public static void main(String[] args) {
