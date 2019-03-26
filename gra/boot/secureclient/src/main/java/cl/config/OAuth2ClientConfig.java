6c4dda8b61b341525c5f8acd6743d838a94ca35d
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-03-26 17:27:02 +0300

    m

--- gra/boot/secureclient/src/main/java/cl/config/OAuth2ClientConfig.java
+++ gra/boot/secureclient/src/main/java/cl/config/OAuth2ClientConfig.java
@@ -9,21 +9,16 @@ import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Configuration
-@Slf4j
 public class OAuth2ClientConfig {
     @Bean
     @ConfigurationProperties("security.oauth2.localas.client")
     public ClientCredentialsResourceDetails localAuthServerResourceDetails() {
-        log.info("enter localAuthServerResourceDetails");
         return new ClientCredentialsResourceDetails();
     }
 
     @Bean
     public OAuth2RestTemplate localAuthServerRestTemplate() {
-        log.info("enter localAuthServerRestTemplate");
         return new OAuth2RestTemplate(localAuthServerResourceDetails());
     }
 }
