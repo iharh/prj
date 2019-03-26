484fac772f8141e786cd7b4a7b4fb3cacdaba086
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-03-26 15:50:40 +0300

    m

--- gra/boot/secureclient/src/main/java/cl/SecureApplication.java
+++ gra/boot/secureclient/src/main/java/cl/SecureApplication.java
@@ -1,10 +1,31 @@
 package cl;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-public class SecureApplication {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+
+//@SpringBootApplication
+@ComponentScan({"cl"})
+public class SecureApplication implements CommandLineRunner {
+    @Autowired
+    OAuth2RestTemplate localT;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (localT == null) {
+            System.out.println("null localT");
+        } else {
+            OAuth2AccessToken accessToken = localT.getAccessToken();
+            System.out.println("obtained access token: " + accessToken.getValue());
+        }
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SecureApplication.class, args);
     }
