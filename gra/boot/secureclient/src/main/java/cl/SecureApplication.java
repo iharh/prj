6aefc54b2351e12b8f0cacd75739b7734dd3e450
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-04-01 17:43:09 +0300

    m

--- gra/boot/secureclient/src/main/java/cl/SecureApplication.java
+++ gra/boot/secureclient/src/main/java/cl/SecureApplication.java
@@ -1,5 +1,7 @@
 package cl;
 
+import cl.iface.HelloService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.boot.CommandLineRunner;
@@ -9,18 +11,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestIntercepter;
+
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@EnableFeignClients
 @Slf4j
 public class SecureApplication implements CommandLineRunner {
     @Autowired
-    OAuth2RestTemplate localT;
+    private OAuth2RestTemplate localT;
 
+    @Autowired
+    private HelloService helloService;
+    
     @Override
     public void run(String... args) throws Exception {
         OAuth2AccessToken accessToken = localT.getAccessToken();
         log.info("obtained access token: {}", accessToken.getValue());
+
+        ResponseEntity<String> helloServiceResponse = helloService.hello();
+        String helloServiceResponseBody = helloServiceResponse.getBody();
+
+        log.info("hello service response body {}", helloServiceResponseBody);
     }
 
     public static void main(String[] args) {
---
+++ gra/boot/secureclient/src/main/java/cl/iface/HelloService.java
@@ -0,0 +1,13 @@
+package cl.iface;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name="helloresource", url="http://localhost:8080")
+public interface HelloService { 
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    ResponseEntity<String> hello();
+}
