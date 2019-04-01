38cf47f132bea4304c86aededbc8d3df4a3e2f39
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-04-01 17:54:03 +0300

    m

--- gra/boot/secureclient/src/main/java/cl/SecureApplication.java
+++ gra/boot/secureclient/src/main/java/cl/SecureApplication.java
@@ -30,8 +30,8 @@ public class SecureApplication implements CommandLineRunner {
     
     @Override
     public void run(String... args) throws Exception {
-        OAuth2AccessToken accessToken = localT.getAccessToken();
-        log.info("obtained access token: {}", accessToken.getValue());
+        //OAuth2AccessToken accessToken = localT.getAccessToken();
+        //log.info("obtained access token: {}", accessToken.getValue());
 
         ResponseEntity<String> helloServiceResponse = helloService.hello();
         String helloServiceResponseBody = helloServiceResponse.getBody();
