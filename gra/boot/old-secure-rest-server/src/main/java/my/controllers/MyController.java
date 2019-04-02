67dce6f2045485ba259e658dfe211895c21622af
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-04-02 17:02:01 +0300

    m

--- gra/boot/old-secure-rest-server/src/main/java/my/controllers/MyController.java
+++ gra/boot/old-secure-rest-server/src/main/java/my/controllers/MyController.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+
 @RestController
 @RequestMapping("/v1")
 public class MyController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @PreAuthorize("#oauth2.hasScope('cmp.lexicons.export')")
     public String hello() {
         return "hello";
     }
