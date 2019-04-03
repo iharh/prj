528b57cda1dac7a1ef6c896e9373a85d2ecca4af
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-04-03 14:22:00 +0300

    m

--- gra/boot/old-secure-rest-server/src/main/java/my/config/ResourceSecurityConfig.java
+++ gra/boot/old-secure-rest-server/src/main/java/my/config/ResourceSecurityConfig.java
@@ -11,35 +11,20 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 
 import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-
-//import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
-//import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Configuration
 @EnableResourceServer
 public class ResourceSecurityConfig extends ResourceServerConfigurerAdapter {
-    /*
-    @Bean
-    @ConfigurationProperties("security.oauth2.localas.resource")
-    public ClientCredentialsResourceDetails localAuthServerResourceDetails() {
-        return new ClientCredentialsResourceDetails();
-    }
-    */
-
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
         resources.resourceId("resource");
     }
 
     @Bean
-    public RemoteTokenServices LocalTokenService() {
-        final RemoteTokenServices tokenService = new RemoteTokenServices();
-        // http://localhost:8091/oauth/token
-        tokenService.setCheckTokenEndpointUrl("http://localhost:8091/oauth/check_token");
-        tokenService.setClientId("nlpsvc");
-        tokenService.setClientSecret("devsecret");
-        return tokenService;
+    @ConfigurationProperties("security.oauth2.localas.resource")
+    public RemoteTokenServices localTokenService() {
+        return new RemoteTokenServices();
     }
 
     @Override
@@ -52,6 +37,4 @@ public class ResourceSecurityConfig extends ResourceServerConfigurerAdapter {
                 .csrf().disable()
         ;
     }
-
-    // new DefaultOAuth2ClientContext()
 }
