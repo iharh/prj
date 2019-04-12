8c8605839d5510c16b501832736631c1e03b448a
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-04-12 12:23:36 +0300

    m

--- gra/boot/simple-rest-server/sh/t.sh
+++ gra/boot/simple-rest-server/sh/t.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -o errexit
-curl -XPOST http://localhost:8080/bench -H "Content-Type: application/json" -d '{ "id" : "id1", "iter" : "10" }'
+curl -XPOST http://localhost:8080/bench -H "Content-Type: application/json" -d '{ "id" : "id2", "iter" : "10" }'
