1ee1d303ed884aeb6bea428c7a47a7f2247bae9c
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-06-14 18:33:30 +0300

    m

--- dub/clb/src/app.d
+++ dub/clb/src/app.d
@@ -48,7 +48,7 @@ string getRespStatus(string content) {
 }
 
 int main(string[] args) {
-    auto langId = "vi"; // ar de en es fr hi id it ko nl pt ro th th tl tr vi zh
+    auto langId = "bn"; // ar bn de en es fr hi id it ko nl pt ro th th tl tr vi zh
     auto prjNameInt = langId ~ "0int";
     auto prjNameExt = langId ~ "0ext";
     auto prjNames = [ prjNameInt , prjNameExt ];
