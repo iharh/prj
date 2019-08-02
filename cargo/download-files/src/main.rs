bb926ffaeac5b806c8fc9bc0c1f0d9fd3e6c91ae
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-08-02 11:46:47 +0300

    m

--- cargo/download-files/src/main.rs
+++ cargo/download-files/src/main.rs
@@ -1,3 +1,20 @@
+//extern crate reqwest;
+use std::io;
+use std::fs::File;
+
+const BASE_URL: &str = "https://nexus.dev.clarabridge.net/content/repositories/public/Clarabridge/FX-windows-x64/1.104.0/";
+
+const MODULE_NAMES: &'static [&'static str] = &["break-detector", "cape", "gazetteer", "morph", "morph5", "morph_tr", "pe",
+    "post-syntax", "regexp", "syntax", "tokenizer_ja", "tokenizer_zh", "unknown", "unkword_en" ];
+
 fn main() {
-    println!("Hello, world!");
+    for module_name in MODULE_NAMES {
+        let file_name = format!("{}.pdb", module_name);
+        let url = format!("{}{}", BASE_URL, file_name);
+
+        println!("downloading {} ...", file_name);
+        let mut resp = reqwest::get(&url).expect("request failed");
+        let mut out = File::create(format!("./out/{}", file_name)).expect("failed to create file");
+        io::copy(&mut resp, &mut out).expect("failed to copy content");
+    }
 }
