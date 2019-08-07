50e3493eacf2a736e5d93020697abdb13cbd9d77
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-08-07 22:22:56 +0300

    m

--- gra/multiproto/impl/src/test/java/org/orgorg/MultiProtoTests.java
+++ gra/multiproto/impl/src/test/java/org/orgorg/MultiProtoTests.java
@@ -2,6 +2,8 @@ package org.orgorg;
 
 import org.junit.jupiter.api.Test;
 
+import org.someorg.serialization.proto.MPService;
+
 import com.google.protobuf.util.JsonFormat;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,6 +11,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MultiProtoTests {
     @Test
     public void test1() throws Exception {
-        assertThat(true).isEqualTo(true);
+        MPService.MPResponse.Builder respBuilder = MPService.MPResponse.newBuilder();
+        respBuilder.setRes("abc");
+        MPService.MPResponse resp = respBuilder.build();
+        assertThat(resp).isNotNull();
+
+        String content = JsonFormat.printer().includingDefaultValueFields().print(resp);
+        assertThat(content).isEqualTo("{\n  \"res\": \"abc\"\n}");
     }
 }
