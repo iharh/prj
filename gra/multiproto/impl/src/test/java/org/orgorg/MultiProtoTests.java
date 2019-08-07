f842f153ff78e046bdf390bbef3bdeec9f3d4f62
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-08-07 16:20:10 +0300

    m

---
+++ gra/multiproto/impl/src/test/java/org/orgorg/MultiProtoTests.java
@@ -0,0 +1,12 @@
+package org.orgorg;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MultiProtoTests {
+    @Test
+    public void test1() throws Exception {
+        assertTrue(true);
+    }
+}
