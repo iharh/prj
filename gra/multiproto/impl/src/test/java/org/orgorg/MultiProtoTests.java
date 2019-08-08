827ae521f6da6dd197997e95974473cc693515c0
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-08-08 13:03:38 +0300

    m

--- gra/multiproto/impl/src/test/java/org/orgorg/MultiProtoTests.java
+++ gra/multiproto/impl/src/test/java/org/orgorg/MultiProtoTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.someorg.serialization.proto.MPService;
 
 import com.google.protobuf.util.JsonFormat;
+import com.google.common.io.BaseEncoding;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,7 +17,16 @@ public class MultiProtoTests {
         MPService.MPResponse resp = respBuilder.build();
         assertThat(resp).isNotNull();
 
-        String content = JsonFormat.printer().includingDefaultValueFields().print(resp);
-        assertThat(content).isEqualTo("{\n  \"res\": \"When's\"\n}");
+        //String content = JsonFormat.printer().includingDefaultValueFields().print(resp);
+        //assertThat(content).isEqualTo("{\n  \"res\": \"When's\"\n}");
+
+        String content = encodeBase64(resp);
+        assertThat(content).isEqualTo("CgZXaGVuJ3M=");
+    }
+
+    private String encodeBase64(MPService.MPResponse res) {
+        byte[] byteArray = res.toByteArray();
+        String output = BaseEncoding.base64().encode(byteArray);
+        return output;
     }
 }
