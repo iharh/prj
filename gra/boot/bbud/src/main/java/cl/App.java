248b0cab78949c6861c3a3917e4f4b91a67190d6
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-09-16 11:18:55 +0300

    m

--- gra/boot/bbud/src/main/java/cl/App.java
+++ gra/boot/bbud/src/main/java/cl/App.java
@@ -3,6 +3,13 @@ package cl;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.agent.ByteBuddyAgent;
+import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
+import net.bytebuddy.implementation.FixedValue;
+
+import static net.bytebuddy.matcher.ElementMatchers.*;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -11,9 +18,21 @@ public class App implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         log.info("app start");
+
+        ByteBuddyAgent.install();
+        new ByteBuddy()
+            .redefine(Simple.class)
+            .method(named("tell"))
+            .intercept(FixedValue.value("Hello Foo Redefined"))
+            .make()
+            .load(
+                Simple.class.getClassLoader(), 
+                ClassReloadingStrategy.fromInstalledAgent()
+            );
+
         Simple s = new Simple();
-        s.tell();
-        log.info("app finish");
+        String ret = s.tell();
+        log.info("app finish: {}", ret);
     }
 
     public static void main(String[] args) {
--- gra/boot/bbud/src/main/java/cl/Simple.java
+++ gra/boot/bbud/src/main/java/cl/Simple.java
@@ -5,7 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Simple {
 
-    public void tell() {
+    public String tell() {
         log.info("start tell");
+        return "abc";
     }
 }
