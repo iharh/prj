5323e6035f7f83af8e8711866c11f5ee30e41822
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-09-16 11:41:54 +0300

    m

--- gra/boot/bbud/src/main/java/cl/App.java
+++ gra/boot/bbud/src/main/java/cl/App.java
@@ -6,7 +6,13 @@ import org.springframework.boot.SpringApplication;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.agent.ByteBuddyAgent;
 import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
-import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.implementation.bind.annotation.Origin;
+import net.bytebuddy.implementation.bind.annotation.RuntimeType;
+import net.bytebuddy.implementation.bind.annotation.SuperCall;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.Callable;
 
 import static net.bytebuddy.matcher.ElementMatchers.*;
 
@@ -14,6 +20,18 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class App implements CommandLineRunner {
+
+    public static class TimingInterceptor {
+        @RuntimeType
+        public static Object intercept(@Origin Method method, @SuperCall Callable<?> callable) throws Exception {
+            long start = System.currentTimeMillis();
+            try {
+                return callable.call();
+            } finally {
+                System.out.println(method + " took " + (System.currentTimeMillis() - start));
+            }
+        }
+    }
 
     @Override
     public void run(String... args) throws Exception {
@@ -23,7 +41,7 @@ public class App implements CommandLineRunner {
         new ByteBuddy()
             .redefine(Simple.class)
             .method(named("tell"))
-            .intercept(FixedValue.value("Hello Foo Redefined"))
+            .intercept(MethodDelegation.to(new TimingInterceptor()))
             .make()
             .load(
                 Simple.class.getClassLoader(), 
