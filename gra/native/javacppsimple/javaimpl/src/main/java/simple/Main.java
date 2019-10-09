b36e17430ab962331dc7ca7eab94d3ccd97253df
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-10-09 14:18:27 +0300

    m

--- gra/native/javacppsimple/javaimpl/src/main/java/simple/Main.java
+++ gra/native/javacppsimple/javaimpl/src/main/java/simple/Main.java
@@ -1,14 +1,19 @@
 package simple;
 
-public class Main {
-    public static void main(String[] args) {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+
+public class Main implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
         try (final SimpleProviderLib.SimpleProviderImpl lib = new SimpleProviderLib.SimpleProviderImpl()) {
             long simpleNum = lib.getSimpleNum();
             System.out.println("getSimpleNum: " + simpleNum);
-        } catch (Exception e) {
-            System.out.println("caught: " + e.getMessage());
-            e.printStackTrace();
-            System.exit(1); 
         }
+    }
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
     }
 }
