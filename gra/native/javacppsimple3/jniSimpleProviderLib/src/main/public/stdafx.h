cc71a3af985dfcdcb39d109e4ea8381059f6e108
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-10-10 08:43:26 +0300

    m

--- gra/native/javacppsimple3/jniSimpleProviderLib/src/main/public/stdafx.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-//#include <boost/noncopyable.hpp>
-
-#include <jni.h>
--- gra/native/javacppsimple3/script/t.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-#export LD_LIBRARY_PATH=build/libs/smallrt/shared:$LD_LIBRARY_PATH
-build/exe/s1/s1
