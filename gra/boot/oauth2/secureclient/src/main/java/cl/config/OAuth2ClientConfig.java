9b0710d1085157350a88218b8ae2998934a208bb
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-12-21 15:10:31 +0300

    m

--- gra/boot/oauth2/secureclient/src/main/java/cl/config/OAuth2ClientConfig.java
+++ gra/boot/oauth2/secureclient/src/main/java/cl/config/OAuth2ClientConfig.java
@@ -18,6 +18,7 @@ import java.util.List;
 
 @Configuration
 public class OAuth2ClientConfig {
+    // https://docs.spring.io/spring-security/oauth/apidocs/org/springframework/security/oauth2/client/resource/OAuth2ProtectedResourceDetails.html
     @Bean
     @ConfigurationProperties("security.oauth2.localas.client")
     public ClientCredentialsResourceDetails localAuthServerResourceDetails() {
@@ -28,11 +29,22 @@ public class OAuth2ClientConfig {
 
     @Bean
     public RequestInterceptor oauth2FeignRequestInterceptor() {
+        // https://github.com/spring-cloud/spring-cloud-security/blob/master/spring-cloud-security/src/main/java/org/springframework/cloud/security/oauth2/client/feign/OAuth2FeignRequestInterceptor.java
         return new OAuth2FeignRequestInterceptor(new DefaultOAuth2ClientContext(), localAuthServerResourceDetails());
     }
 
     //@Bean
     //public OAuth2RestTemplate localAuthServerRestTemplate() {
     //    return new OAuth2RestTemplate(localAuthServerResourceDetails());
+    //}
+
+    //@Bean
+    //public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
+    //    return new BasicAuthRequestInterceptor("nlpsvc", "devsecret");
+    //}
+
+    //@Bean
+    //Logger.Level feignLoggerLevel() {
+    //    return Logger.Level.FULL;
     //}
 }
