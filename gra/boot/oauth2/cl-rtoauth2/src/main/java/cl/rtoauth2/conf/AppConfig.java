1198d10e44f62ffe1d3ac7b074140be6a81ddcda
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-12-23 17:26:39 +0300

    m

--- gra/boot/oauth2/cl-rtoauth2/src/main/java/cl/rtoauth2/conf/AppConfig.java
+++ gra/boot/oauth2/cl-rtoauth2/src/main/java/cl/rtoauth2/conf/AppConfig.java
@@ -1,41 +1,35 @@
 package cl.rtoauth2.conf;
 
-// import com.clarabridge.ingestion.messagerouter.spring.CompressingClientHttpRequestInterceptor;
-
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-// import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
 // import org.springframework.web.client.RestTemplate;
-
-import java.util.Arrays;
-// import java.util.Collections;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @Slf4j
 public class AppConfig {
-    // private final ClientHttpRequestInterceptor ribbonInterceptor;
-
-    //public ApplicationConfiguration(ClientHttpRequestInterceptor ribbonInterceptor) {
-    //    this.ribbonInterceptor = ribbonInterceptor;
-    //}
+    @Bean
+    @ConfigurationProperties("security.oauth2.clb.client")
+    public OAuth2ProtectedResourceDetails clbAuthServerResourceDetails() {
+        return new ClientCredentialsResourceDetails();
+    }
 
     @Bean
-    public OAuth2RestTemplate restTemplate(OAuth2ProtectedResourceDetails resource, OAuth2ClientContext clientContext) {
-        log.info("enter restTemplate");
-
+    public OAuth2RestTemplate restTemplate(OAuth2ClientContext clientContext) {
+        final OAuth2ProtectedResourceDetails resource = clbAuthServerResourceDetails();
+        log.info("enter restTemplate resource class: {}", resource.getClass().getName());
         ClientCredentialsAccessTokenProvider tokenProvider = new ClientCredentialsAccessTokenProvider();
-        // tokenProvider.setInterceptors(Collections.singletonList(ribbonInterceptor));
 
         OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resource, clientContext);
         restTemplate.setAccessTokenProvider(tokenProvider);
-        // restTemplate.setInterceptors(Arrays.asList(ribbonInterceptor, new CompressingClientHttpRequestInterceptor()));
         restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
         return restTemplate;
     }
