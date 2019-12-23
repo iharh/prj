d1e34c89dd55b2a198c00e63b94961e171a1e1a2
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-12-23 10:47:54 +0300

    m

---
+++ gra/boot/oauth2/cl-rtoauth2/build.gradle.kts
@@ -0,0 +1,67 @@
+import org.apache.tools.ant.filters.ReplaceTokens
+
+val springCloudVersion = "Hoxton.RELEASE"
+
+plugins {
+    java
+    idea
+    // application
+    id("org.springframework.boot") version "2.2.2.RELEASE"
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
+}
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation("com.google.guava:guava:28.0-jre")
+
+    annotationProcessor("org.projectlombok:lombok")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    compileOnly("org.projectlombok:lombok")
+
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.cloud:spring-cloud-starter-oauth2")
+
+    // implementation("org.springframework.boot:spring-boot-starter-security")
+    // implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    // implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.1")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion") {
+            bomProperty("spring-security-oauth2-autoconfigure.version", "2.2.1.RELEASE")
+        }
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks {
+    withType<JavaCompile> {
+        options.isDeprecation = true
+    }
+    withType<ProcessResources> {
+        filesMatching("/**/bootstrap.yml") {
+            val filterTokens = mapOf(Pair("version", project.version))
+            filter<ReplaceTokens>("tokens" to filterTokens)
+        }
+    }
+    getByName("compileJava").dependsOn("processResources")
+}
+
+// application { mainClassName = "cl.rtoauth2.App" }
+
+val test by tasks.getting(Test::class) {
+    useJUnitPlatform()
+}
