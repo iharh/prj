0920150d9ca749e36383ff8dbab64f87a12c7de2
Ihar Hancharenka <iigoncharenko@gmail.com>
2019-12-27 18:18:38 +0300

    m

--- gra/boot/aws/simpleclient/src/main/java/cl/App.java
+++ gra/boot/aws/simpleclient/src/main/java/cl/App.java
@@ -4,11 +4,33 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+import java.util.List;
+
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @Slf4j
 public class App implements CommandLineRunner {
+    private static final String S3_ENDPOINT = "http://localhost:9000";
+    private static final String S3_REGION = Regions.US_EAST_1.name();
+    private static final String S3_BUCKET = "clbbucket";
+    
     public String getGreeting() {
         return "Hello world.";
     }
@@ -16,6 +38,48 @@ public class App implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         log.info("greeting: {}", getGreeting());
+
+        AWSCredentials credentials = new BasicAWSCredentials("nlpsvc", "devsecret");
+
+        ClientConfiguration clientConfiguration = new ClientConfiguration();
+        clientConfiguration.setSignerOverride("AWSS3V4SignerType");
+
+        AwsClientBuilder.EndpointConfiguration endpointConfiguration =
+                new AwsClientBuilder.EndpointConfiguration(S3_ENDPOINT, S3_REGION);
+        
+        AmazonS3 s3Client = AmazonS3ClientBuilder
+                .standard()
+                .withEndpointConfiguration(endpointConfiguration)
+                .withPathStyleAccessEnabled(true)
+                .withClientConfiguration(clientConfiguration)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+
+        try {
+            ObjectListing listing = s3Client.listObjects(S3_BUCKET);
+            List<S3ObjectSummary> summaries = listing.getObjectSummaries();
+
+            while (listing.isTruncated()) {
+                listing = s3Client.listNextBatchOfObjects(listing);
+                summaries.addAll(listing.getObjectSummaries());
+            }
+
+            summaries.stream()
+                .map(S3ObjectSummary::getKey)
+                .forEach(System.out::println);
+            
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which means your request made it to Amazon S3, but was rejected with an error response for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which means the client encountered an internal error while trying to communicate with S3, such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
     }
 
     public static void main(String[] args) {
