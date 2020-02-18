6ec536a9d3b39ae1788728ba7b963028d88b3559
Ihar Hancharenka <iigoncharenko@gmail.com>
2020-02-18 22:11:18 +0300

    m

---
+++ gra/boot/oauth2/cl-resrep/build.gradle.kts
@@ -0,0 +1,56 @@
+import org.apache.tools.ant.filters.ReplaceTokens
+
+plugins {
+    java
+    idea
+    id("org.springframework.boot") version "2.2.4.RELEASE"
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
+}
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation("com.google.guava:guava:28.0-jre")
+
+    annotationProcessor("org.projectlombok:lombok")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    compileOnly("org.projectlombok:lombok")
+
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.security.oauth.boot:spring-security-oauth2-autoconfigure")
+
+    //implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.apache.httpcomponents:httpclient:4.5.10")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.1")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks {
+    withType<JavaCompile> {
+        options.isDeprecation = true
+    }
+    withType<ProcessResources> {
+        filesMatching("/**/bootstrap.yml") {
+            val filterTokens = mapOf(Pair("version", project.version))
+            filter<ReplaceTokens>("tokens" to filterTokens)
+        }
+    }
+    test {
+        useJUnitPlatform()
+        testLogging {
+            events("PASSED", "FAILED", "SKIPPED")
+            showStandardStreams = false
+        }
+    }
+    getByName("compileJava").dependsOn("processResources")
+}
--- gra/boot/oauth2/cl-rtoauth2/src/test/java/cl/rtoauth2/AppTest.java
+++ gra/boot/oauth2/cl-rtoauth2/src/test/java/cl/rtoauth2/AppTest.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class AppTest {
-    @Test void appHasAGreeting() {
+    @Test
+    void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
