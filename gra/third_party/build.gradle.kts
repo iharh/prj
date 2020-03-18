8ce05b6880db4fabd15f5e6ef1261a054b0c8b61
Ihar Hancharenka <iigoncharenko@gmail.com>
2020-03-18 22:48:58 +0300

    m

--- gra/third_party/build.gradle.kts
+++ gra/third_party/build.gradle.kts
@@ -1,6 +1,14 @@
+plugins {
+    id("de.undercouch.download").version("4.0.4").apply(false)
+}
+
 allprojects {
     repositories {
         mavenCentral()
         jcenter()
     }
+}
+
+subprojects {
+    apply(plugin = "de.undercouch.download")
 }
--- gra/third_party/xerces-c/build.gradle.kts
+++ gra/third_party/xerces-c/build.gradle.kts
@@ -1,7 +1,15 @@
+import de.undercouch.gradle.tasks.download.Download
+
 tasks {
+    val downloadZip by registering(Download::class) {
+        src("https://github.com/michel-kraemer/gradle-download-task/archive/1.0.zip")
+        dest(File(buildDir, "1.0.zip"))
+    }
+
     create("abc") {
         doLast {
             println("hello from abc")
+            // println("Property: ${project.extra["myNewProperty"]}") 
         }
     }
 }
