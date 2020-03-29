a49165bec944f47a0038dc83a2df771ac4c0055d
Ihar Hancharenka <iigoncharenko@gmail.com>
2020-03-30 00:01:50 +0300

    m

--- gra/native/smallvec/src/main/cpp/main.cpp
+++ gra/native/smallvec/src/main/cpp/main.cpp
@@ -1,10 +1,55 @@
+#include "SmallVector.h"
+
 #include <iostream>
+
+struct my_data
+{
+    my_data()
+    {
+    }
+
+    int m_i;
+    long m_l;
+    float m_f;
+};
+
+typedef std::shared_ptr<my_data> my_data_ptr;
 
 int
 main(void)
 {
-    int *pI = new int;
-    *pI = 3;
-    std::wcout << L"Hello Smallvec: " << *pI << std::endl;
+    typedef llvm::SmallVector<my_data_ptr, 64> t_small_vec;
+
+    t_small_vec vActiveInstances, vTmpActiveInstances;
+
+    std::wcout << L"Hello Smallvec: 1" << std::endl;
+
+    vActiveInstances.push_back(std::make_shared<my_data>());
+
+    std::wcout << L"Hello Smallvec: 2" << std::endl;
+
+    for (size_t j = 0; j < 69; ++j)
+    {
+        vTmpActiveInstances.push_back(std::make_shared<my_data>());
+    }
+
+    std::wcout << L"Hello Smallvec: 3" << std::endl;
+
+    vActiveInstances.swap(vTmpActiveInstances); // grow called
+
+    std::wcout << L"Hello Smallvec: 4" << std::endl;
+
+    vTmpActiveInstances.clear();
+
+    std::wcout << L"Hello Smallvec: 5" << std::endl;
+
+    vActiveInstances.swap(vTmpActiveInstances);
+
+    std::wcout << L"Hello Smallvec: 6" << std::endl;
+
+    vTmpActiveInstances.clear();
+
+    std::wcout << L"Hello Smallvec: 7" << std::endl;
+
     return 0;
 }
