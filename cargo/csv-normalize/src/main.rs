566af9ca7bb1614996a0113d8c9966657185a63a
Ihar Hancharenka <iigoncharenko@gmail.com>
2020-09-25 11:46:26 +0300

    m

--- cargo/csv-normalize/src/main.rs
+++ cargo/csv-normalize/src/main.rs
@@ -1,5 +1,27 @@
-extern crate csv;
+// extern crate csv;
+
+use std::error::Error;
+use std::io;
+use std::process;
+
+fn print_records() -> Result<(), Box<dyn Error>> {
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+    
+    // The iterator yields Result<StringRecord, Error>
+    for result in rdr.records() {
+        let record = result?;
+        let extracted = &record[0];
+        let single_line = str::replace(extracted, "\n", " ");
+        let s = single_line.trim();
+        println!("{}", s);
+    }
+
+    Ok(())
+}
 
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = print_records() {
+        println!("error printing records: {}", err);
+        process::exit(1);
+    }
 }
