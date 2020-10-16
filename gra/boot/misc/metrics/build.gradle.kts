f744b8bcba3d8e0fc568164132db5f8e85c88cd4
Ihar Hancharenka <iigoncharenko@gmail.com>
2020-10-16 10:27:13 +0300

    m

--- gra/boot/misc/metrics/build.gradle.kts
+++ gra/boot/misc/metrics/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-autoconfigure")
     implementation("org.springframework.boot:spring-boot-starter")
-    // implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     implementation("io.micrometer:micrometer-registry-prometheus")
 
--- gra/boot/misc/metrics/src/main/java/metrics/MetricsRunner.java
+++ gra/boot/misc/metrics/src/main/java/metrics/MetricsRunner.java
@@ -4,12 +4,15 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Component
+@Slf4j
 public class MetricsRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("Hello metrics!");
+        log.info("Hello metrics!");
     }
 
 }
