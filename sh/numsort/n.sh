3bb2bf17815bd0839a822f860c93f6cbfaf7e90a
Ihar Hancharenka <iigoncharenko@gmail.com>
2020-12-08 20:50:09 +0300

    m

--- sh/numsort/n.sh
+++ sh/numsort/n.sh
@@ -1,3 +1,6 @@
 #! /usr/bin/env bash
 CUR_ITER=`cat n.txt | cut -c18- | sort -n -r | head -n 1`
-echo CUR_ITER: $CUR_ITER
+echo CUR_ITER: $CUR_ITER. Enter var: 
+#local VAR
+read -r VAR
+echo VAR: [$VAR]
