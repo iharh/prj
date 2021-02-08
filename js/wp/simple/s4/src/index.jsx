8a942ba44cd6af57a8f6d5cd7e5f69fb37b9ce97
Ihar Hancharenka <iigoncharenko@gmail.com>
2021-02-08 18:42:46 +0300

    m

--- js/wp/simple/s4/src/index.jsx
+++ js/wp/simple/s4/src/index.jsx
@@ -1,15 +1,9 @@
-// https://medium.com/the-node-js-collection/modern-javascript-explained-for-dinosaurs-f695e9747b70
+import { moment } from "moment";
 import ReactDOM from "react-dom";
-var moment = require('moment');
 
 console.log('before hello from main file!');
 console.log(moment().startOf('day').fromNow());
 console.log('after hello from main file!');
-
-// import React, {FC} from "react";
-// function App() {
-//  return <h1>Hello World</h1>;
-// }
 
 const App = () => {
   return(
@@ -19,4 +13,4 @@ const App = () => {
   )
 }
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App/>, document.getElementById("root"))
