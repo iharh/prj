9420706098772c75fc6f7b4c75df2b2f9990b8ae
Ihar Hancharenka <iigoncharenko@gmail.com>
2021-05-18 16:50:28 +0300

    m

--- gra/publish/with-dep-graph/pant/build.gradle.kts
+++ gra/publish/with-dep-graph/pant/build.gradle.kts
@@ -5,14 +5,16 @@ ant.lifecycleLogLevel = org.gradle.api.AntBuilder.AntMessagePriority.INFO
 val localNexusUsername: String by project
 val localNexusPassword: String by project
 val localNexusRealm: String by project
+val localNexusHost: String by project
 val localNexusRepoPublishSnapshots: String by project
 
 tasks {
     create("antPublishNexus") {
         doLast {
-            ant.setProperty("nexus.deploy.user", nexusUsername)
-            ant.setProperty("nexus.deploy.password", nexusPassword)
-            ant.setProperty("nexus.realm", nexusRealm)
+            ant.setProperty("nexus.deploy.user", localNexusUsername)
+            ant.setProperty("nexus.deploy.password", localNexusPassword)
+            ant.setProperty("nexus.realm", localNexusRealm)
+            ant.setProperty("nexus.host", localNexusHost)
             ant.setProperty("nexus.repo.snapshots", localNexusRepoPublishSnapshots)
             // ant.project.executeTarget("dump-pub-cfg")
             ant.project.executeTarget("pub-to-nexus")
