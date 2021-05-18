6acc93c2447c48d0cc9915df6e277c8e2726ae01
Ihar Hancharenka <iigoncharenko@gmail.com>
2021-05-18 16:36:48 +0300

    m

--- gra/publish/with-dep-graph/pant/build.gradle.kts
+++ gra/publish/with-dep-graph/pant/build.gradle.kts
@@ -2,9 +2,19 @@ ant.importBuild("$projectDir/build.xml") { antTargetName -> "ant-target-" + antT
 
 ant.lifecycleLogLevel = org.gradle.api.AntBuilder.AntMessagePriority.INFO
 
+val localNexusUsername: String by project
+val localNexusPassword: String by project
+val localNexusRealm: String by project
+val localNexusRepoPublishSnapshots: String by project
+
 tasks {
     create("antPublishNexus") {
         doLast {
+            ant.setProperty("nexus.deploy.user", nexusUsername)
+            ant.setProperty("nexus.deploy.password", nexusPassword)
+            ant.setProperty("nexus.realm", nexusRealm)
+            ant.setProperty("nexus.repo.snapshots", localNexusRepoPublishSnapshots)
+            // ant.project.executeTarget("dump-pub-cfg")
             ant.project.executeTarget("pub-to-nexus")
         }
     }
